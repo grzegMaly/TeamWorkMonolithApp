76d8759b13ce1e25677e95cc33d4f19a604ed401
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-05 17:23:32 +0100

    Controllers Setup

---
+++ backend/src/main/java/com/mordiniaa/backend/controllers/authControllers/AuthController.java
@@ -0,0 +1,9 @@
+package com.mordiniaa.backend.controllers.authControllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+}
---
+++ backend/src/main/java/com/mordiniaa/backend/controllers/notesController/NotesController.java
@@ -0,0 +1,9 @@
+package com.mordiniaa.backend.controllers.notesController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/notes")
+public class NotesController {
+}
