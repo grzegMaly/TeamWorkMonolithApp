f1fd0658e31853ab92e2376cf04ba17703d8b744
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-06 16:22:12 +0100

    Auditing

---
+++ backend/src/main/java/com/mordiniaa/backend/audit/mongo/MongoAuditConfig.java
@@ -0,0 +1,8 @@
+package com.mordiniaa.backend.audit.mongo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+public class MongoAuditConfig {
+}
