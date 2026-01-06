1c546605c3dfa111e172beae8521a5b04d6fedba
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-06 13:43:08 +0100

    MongoDB Properties Setup

---
+++ backend/init.js
@@ -0,0 +1,23 @@
+use team-work;
+
+db.createUser({
+    user: "team-work-admin",
+    roles: [
+        {role: "dbAdmin", db: "team-work"},
+        {role: "dbOwner", db: "team-work"},
+        {role: "userAdmin", db: "team-work"}
+    ],
+    pwd: "admin"
+});
+
+db.auth("team-work-admin", "admin")
+
+use team-work
+
+db.createUser({
+    user: "team-work-dev",
+    roles: [
+        {role: "readWrite", db: "team-wok"}
+    ],
+    pwd: "password"
+})
