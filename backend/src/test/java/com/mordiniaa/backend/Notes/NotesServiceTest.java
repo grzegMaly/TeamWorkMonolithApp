3b613c199f4cab4c97f74495c1b5fb27ca6d0cd1
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-07 00:49:52 +0100

    Mappers for Notes To DTO Tested

--- backend/src/test/java/com/mordiniaa/backend/Notes/NotesServiceTest.java
+++ backend/src/test/java/com/mordiniaa/backend/Notes/NotesServiceTest.java
@@ -1,5 +1,6 @@
-package com.mordiniaa.backend;
+package com.mordiniaa.backend.Notes;
 
+import com.mordiniaa.backend.BackendApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = BackendApplication.class)
