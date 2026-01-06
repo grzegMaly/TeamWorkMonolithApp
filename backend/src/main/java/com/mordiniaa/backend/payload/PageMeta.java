558ce70d35199adeba941241a4c1a940ee1bd46f
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-06 16:46:48 +0100

    Response Payload Implementation

---
+++ backend/src/main/java/com/mordiniaa/backend/payload/PageMeta.java
@@ -0,0 +1,18 @@
+package com.mordiniaa.backend.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PageMeta {
+
+    private int page;
+    private int size;
+    private long totalItems;
+    private int totalPages;
+}
