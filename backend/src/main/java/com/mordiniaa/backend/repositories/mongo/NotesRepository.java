c08c23903c376b4bdcba4e02f00d215ba023af5c
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-08 00:08:56 +0100

    Pagination and Criteria Improvement
    Extending existing method for pagination and criteria searching

--- backend/src/main/java/com/mordiniaa/backend/repositories/mongo/NotesRepository.java
+++ backend/src/main/java/com/mordiniaa/backend/repositories/mongo/NotesRepository.java
@@ -1,18 +1,21 @@
 package com.mordiniaa.backend.repositories.mongo;
 
+import com.mongodb.lang.Nullable;
 import com.mordiniaa.backend.models.notes.Note;
 import org.bson.types.ObjectId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.core.query.TextCriteria;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface NotesRepository extends MongoRepository<Note, ObjectId> {
 
-    List<Note> findAllByOwnerId(UUID ownerId);
+    Page<Note> findAllByOwnerId(UUID ownerId, Pageable pageable, @Nullable TextCriteria textCriteria);
 
     Optional<Note> findNoteByIdAndOwnerId(ObjectId id, UUID ownerId);
 }
