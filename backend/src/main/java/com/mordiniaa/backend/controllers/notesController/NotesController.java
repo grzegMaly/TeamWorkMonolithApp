e4057a7d18ac3dafc4ae442d1f84ef8bca9ef306
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-08 00:10:28 +0100

    Controllers for page search implemented

--- backend/src/main/java/com/mordiniaa/backend/controllers/notesController/NotesController.java
+++ backend/src/main/java/com/mordiniaa/backend/controllers/notesController/NotesController.java
@@ -5,10 +5,12 @@ import com.mordiniaa.backend.dto.NoteDto;
 import com.mordiniaa.backend.payload.ApiResponse;
 import com.mordiniaa.backend.payload.CollectionResponse;
 import com.mordiniaa.backend.services.notes.NotesService;
+import com.mordiniaa.backend.utils.PageResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -18,17 +20,6 @@ public class NotesController {
 
     private final NotesService notesService;
 
-    @GetMapping
-    public ResponseEntity<CollectionResponse<NoteDto>> fetchAllNotesForUser(
-            @RequestParam(name = "pn", required = false, defaultValue = NotesConstants.PAGE_NUMBER) int pageNumber,
-            @RequestParam(name = "ps", required = false, defaultValue = NotesConstants.PAGE_SIZE) int pageSize,
-            @RequestParam(name = "pso", required = false, defaultValue = NotesConstants.SORT_ORDER) String sortOrder,
-            @RequestParam(name = "psk", required = false, defaultValue = "") String sortKey,
-            @RequestParam(name = "key", required = false, defaultValue = "") String keyword
-    ) {
-        return null;
-    }
-
     @GetMapping("/{noteId}")
     public ResponseEntity<ApiResponse<NoteDto>> getNoteById(@PathVariable String noteId) {
         return notesService.getNoteById(
@@ -37,6 +28,27 @@ public class NotesController {
                 )
                 .map(dto -> ResponseEntity.ok(new ApiResponse<>("Success", dto)))
                 .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<CollectionResponse<NoteDto>> fetchAllNotesForUser(
+            @RequestParam(name = "pn", required = false, defaultValue = NotesConstants.PAGE_NUMBER) int pageNumber,
+            @RequestParam(name = "ps", required = false, defaultValue = NotesConstants.PAGE_SIZE) int pageSize,
+            @RequestParam(name = "pso", required = false, defaultValue = NotesConstants.SORT_ORDER) String sortOrder,
+            @RequestParam(name = "psk", required = false, defaultValue = "id") String sortKey,
+            @RequestParam(name = "key", required = false, defaultValue = "") String keyword
+    ) {
+        PageResult<List<NoteDto>> result = notesService.fetchAllNotesForUser(UUID.randomUUID(), //TODO: Get id from user in security section
+                pageNumber, pageSize, sortOrder, sortKey, keyword);
+        return ResponseEntity.ok(new CollectionResponse<>(result.getData(), result.getPageMeta()));
+    }
+
+    @GetMapping("/autocomplete")
+    public ResponseEntity<CollectionResponse<NoteDto>> getAutocompleteSuggestions(
+            @RequestParam(name = "pn", required = false, defaultValue = NotesConstants.PAGE_NUMBER) int pageNumber,
+            @RequestParam(name = "key", required = false, defaultValue = "") String keyword
+    ) {
+        return null;
     }
 
     @PostMapping
