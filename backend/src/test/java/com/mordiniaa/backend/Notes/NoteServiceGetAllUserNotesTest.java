0d317bd7d01a37c371f926aafe2d6f016841868e
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-08 16:12:33 +0100

    Testing Properties

---
+++ backend/src/test/java/com/mordiniaa/backend/Notes/NoteServiceGetAllUserNotesTest.java
@@ -0,0 +1,158 @@
+package com.mordiniaa.backend.Notes;
+
+import com.mordiniaa.backend.BackendApplication;
+import com.mordiniaa.backend.dto.NoteDto;
+import com.mordiniaa.backend.mappers.notes.NoteMapper;
+import com.mordiniaa.backend.models.notes.Note;
+import com.mordiniaa.backend.models.notes.deadline.DeadlineNote;
+import com.mordiniaa.backend.models.notes.deadline.Priority;
+import com.mordiniaa.backend.models.notes.deadline.Status;
+import com.mordiniaa.backend.models.notes.regular.Category;
+import com.mordiniaa.backend.models.notes.regular.RegularNote;
+import com.mordiniaa.backend.payload.PageMeta;
+import com.mordiniaa.backend.repositories.mongo.NotesRepository;
+import com.mordiniaa.backend.services.notes.NotesServiceImpl;
+import com.mordiniaa.backend.utils.PageResult;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles("test")
+@SpringBootTest(classes = BackendApplication.class)
+public class NoteServiceGetAllUserNotesTest {
+
+    @Autowired
+    private NotesServiceImpl notesService;
+
+    @Autowired
+    private NotesRepository notesRepository;
+
+    private static UUID ownerId;
+
+    @BeforeEach
+    void setup() {
+
+        Random random = new Random();
+        List<Note> storedNotes = new ArrayList<>();
+
+        ownerId = UUID.randomUUID();
+        String baseTitle = "%s Base Title";
+        String baseContent = "%s Base Content";
+        Instant baseCreatedAt = Instant.now().minus(10, ChronoUnit.DAYS);
+        Instant baseUpdatedAt = Instant.now().minus(5, ChronoUnit.DAYS);
+
+        for (int i = 0; i < 30; i++) {
+            Note note;
+            String formattedTitle = baseTitle.formatted(i < 10 ? "0" + i : i);
+            String formattedContent = baseContent.formatted(i < 10 ? "0" + i : i);
+
+            if (random.nextBoolean()) {
+                note = new RegularNote();
+                note.setOwnerId(ownerId);
+                note.setTitle(formattedTitle);
+                note.setContent(formattedContent);
+                note.setArchived(random.nextBoolean());
+                ((RegularNote) note).setCategory(Category.values()[random.nextInt(Category.values().length)]);
+                note.setCreatedAt(baseCreatedAt);
+                note.setUpdatedAt(baseUpdatedAt);
+                storedNotes.add(note);
+            } else {
+                note = new DeadlineNote();
+
+                Instant deadline = Instant.now().plus(random.nextInt(1, 10), ChronoUnit.DAYS);
+
+                note.setOwnerId(ownerId);
+                note.setTitle(formattedTitle);
+                note.setContent(formattedContent);
+                note.setArchived(random.nextBoolean());
+                ((DeadlineNote) note).setPriority(Priority.values()[random.nextInt(Priority.values().length)]);
+                ((DeadlineNote) note).setStatus(Status.values()[random.nextInt(Status.values().length)]);
+                note.setCreatedAt(baseCreatedAt);
+                note.setUpdatedAt(baseUpdatedAt);
+                ((DeadlineNote) note).setDeadline(deadline);
+                storedNotes.add(note);
+            }
+        }
+        notesRepository.saveAll(storedNotes);
+    }
+
+//    @AfterEach
+    void clear() {
+        notesRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("Pagination Test")
+    void getAllNotesForUser() {
+
+        PageResult<List<NoteDto>> pageResult = notesService.fetchAllNotesForUser(ownerId, 0, 5, "asc", "id", null);
+
+        PageMeta meta = pageResult.getPageMeta();
+        assertEquals(6, meta.getTotalPages());
+        assertFalse(meta.isLastPage());
+
+        pageResult = notesService.fetchAllNotesForUser(ownerId, 5, 5, "asc", "id", null);
+        meta = pageResult.getPageMeta();
+        assertEquals(5, meta.getSize());
+        assertEquals(5, meta.getPage());
+        assertTrue(meta.isLastPage());
+
+        pageResult = notesService.fetchAllNotesForUser(ownerId, 0, 25, "asc", "id", null);
+        meta = pageResult.getPageMeta();
+        assertEquals(25, meta.getSize());
+        assertEquals(0, meta.getPage());
+        assertEquals(30, meta.getTotalItems());
+        assertFalse(meta.isLastPage());
+    }
+
+    @Test
+    @DisplayName("Title sort test")
+    void titleSortTest() {
+        PageResult<List<NoteDto>> pageResult = notesService.fetchAllNotesForUser(ownerId, 0, 10, "asc", "title", null);
+        List<NoteDto> data = pageResult.getData();
+        List<Integer> numbers = data.stream()
+                .map(NoteDto::getTitle)
+                .map(title -> Integer.valueOf(title.split(" ")[0]))
+                .toList();
+
+        for (int i = 0; i < numbers.size(); i++) {
+            if (i != numbers.size() - 1) {
+                assertTrue(numbers.get(i) < numbers.get(i + 1));
+            }
+        }
+
+        pageResult = notesService.fetchAllNotesForUser(ownerId, 0, 10, "desc", "title", null);
+        data = pageResult.getData();
+        data.forEach(dto -> System.out.println(dto.getTitle()));
+        numbers = data.stream()
+                .map(NoteDto::getTitle)
+                .map(title -> Integer.valueOf(title.split(" ")[0]))
+                .toList();
+
+        for (int i = 0; i < numbers.size(); i++) {
+            if (i != numbers.size() - 1) {
+                assertTrue(numbers.get(i) > numbers.get(i + 1));
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("Test Text Criteria")
+    void textCriteriaTest() {
+        PageResult<List<NoteDto>> pageResult = notesService.fetchAllNotesForUser(ownerId, 0, 10, "asc", "id", "3");
+        List<NoteDto> data = pageResult.getData();
+
+        assertEquals(4, data.size());
+    }
+}
