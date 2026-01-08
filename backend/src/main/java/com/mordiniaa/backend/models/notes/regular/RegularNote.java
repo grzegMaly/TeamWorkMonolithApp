f872a3d134e0acd231deb793f43f60073474c782
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-08 13:35:49 +0100

    ToString addition for debug

--- backend/src/main/java/com/mordiniaa/backend/models/notes/regular/RegularNote.java
+++ backend/src/main/java/com/mordiniaa/backend/models/notes/regular/RegularNote.java
@@ -4,6 +4,7 @@ package com.mordiniaa.backend.models.notes.regular;
 import com.mordiniaa.backend.models.notes.Note;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
@@ -11,6 +12,7 @@ import org.springframework.data.mongodb.core.mapping.Field;
 @Getter
 @Setter
 @TypeAlias("regular")
+@ToString(callSuper = true)
 @Document(collection = "notes")
 public class RegularNote extends Note {
 
