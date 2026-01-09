41702194dc6038732cf7210ed6113b89bb2d7587
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-09 22:58:56 +0100

    Test Setup

---
+++ backend/src/test/java/com/mordiniaa/backend/Notes/NoteMapperToModelTest.java
@@ -0,0 +1,63 @@
+package com.mordiniaa.backend.Notes;
+
+import com.mordiniaa.backend.mappers.notes.NoteMapper;
+import com.mordiniaa.backend.mappers.notes.modelMappers.DeadlineNoteModelMapper;
+import com.mordiniaa.backend.mappers.notes.modelMappers.RegularNoteModelMapper;
+import com.mordiniaa.backend.models.notes.deadline.DeadlineNote;
+import com.mordiniaa.backend.models.notes.deadline.Priority;
+import com.mordiniaa.backend.models.notes.deadline.Status;
+import com.mordiniaa.backend.models.notes.regular.Category;
+import com.mordiniaa.backend.models.notes.regular.RegularNote;
+import com.mordiniaa.backend.request.note.deadline.CreateDeadlineNoteRequest;
+import com.mordiniaa.backend.request.note.regular.CreateRegularNoteRequest;
+import com.mordiniaa.backend.request.note.regular.PatchRegularNoteRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {
+        NoteMapper.class,
+        RegularNoteModelMapper.class,
+        DeadlineNoteModelMapper.class
+})
+public class NoteMapperToModelTest {
+
+    @Autowired
+    private NoteMapper noteMapper;
+
+    private CreateRegularNoteRequest createRegularNoteRequest;
+    private CreateDeadlineNoteRequest createDeadlineNoteRequest;
+
+    private final String title = "My Custom Title";
+    private final String content = "My Custom Content";
+    private final Category category = Category.DIARY;
+    private final Status status = Status.CANCELED;
+    private final Priority priority = Priority.LOW;
+    private final Instant deadline = Instant.now().plus(4, ChronoUnit.DAYS);
+
+    @BeforeEach
+    void setup() {
+
+        createRegularNoteRequest = new CreateRegularNoteRequest();
+        createRegularNoteRequest.setTitle(title);
+        createRegularNoteRequest.setContent(content);
+        createRegularNoteRequest.setCategory(category);
+
+        createDeadlineNoteRequest = new CreateDeadlineNoteRequest();
+        createDeadlineNoteRequest.setTitle(title);
+        createDeadlineNoteRequest.setContent(content);
+        createDeadlineNoteRequest.setStatus(status);
+        createDeadlineNoteRequest.setPriority(priority);
+        createDeadlineNoteRequest.setDeadline(deadline);
+    }
+}
