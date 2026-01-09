d6b705c9b8623ac41d6d586a3f8060a7e74e7ebf
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-09 22:18:42 +0100

    Test Renamed

--- backend/src/test/java/com/mordiniaa/backend/Notes/NotesMapperToDtoTest.java
+++ backend/src/test/java/com/mordiniaa/backend/Notes/NotesMapperToDtoTest.java
@@ -31,7 +31,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
         RegularNoteDtoMapper.class,
         DeadlineNoteDtoMapper.class
 })
-public class NotesMapperTest {
+public class NotesMapperToDtoTest {
 
     @Autowired
     private NoteMapper noteMapper;
