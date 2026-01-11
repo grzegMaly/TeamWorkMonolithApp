28e6e02bbbfc12988481e5cd078ca6a8dcf1c9b8
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-12 00:28:53 +0100

    Set Archived True Test

--- backend/src/test/java/com/mordiniaa/backend/Notes/serviceRepo/archived/ArchivedNoteServiceSwitchArchivedTest.java
+++ backend/src/test/java/com/mordiniaa/backend/Notes/serviceRepo/archived/ArchivedNoteServiceSwitchArchivedTest.java
@@ -1,9 +1,15 @@
 package com.mordiniaa.backend.Notes.serviceRepo.archived;
 
+import com.mordiniaa.backend.mappers.notes.NoteMapper;
+import com.mordiniaa.backend.mappers.notes.dtoMappers.DeadlineNoteDtoMapper;
+import com.mordiniaa.backend.mappers.notes.dtoMappers.RegularNoteDtoMapper;
+import com.mordiniaa.backend.models.notes.Note;
 import com.mordiniaa.backend.models.notes.regular.Category;
 import com.mordiniaa.backend.models.notes.regular.RegularNote;
 import com.mordiniaa.backend.repositories.mongo.NotesRepository;
 import com.mordiniaa.backend.services.notes.ArchivedNotesServiceImpl;
+import com.mordiniaa.backend.services.notes.NotesServiceImpl;
+import org.bson.types.ObjectId;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -12,17 +18,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.util.Optional;
+import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ActiveProfiles("test")
 @DataMongoTest
 @Import({
-        ArchivedNotesServiceImpl.class
+        ArchivedNotesServiceImpl.class,
+        NotesServiceImpl.class,
+        NoteMapper.class,
+        RegularNoteDtoMapper.class,
+        DeadlineNoteDtoMapper.class
 })
 public class ArchivedNoteServiceSwitchArchivedTest {
 
@@ -35,6 +50,12 @@ public class ArchivedNoteServiceSwitchArchivedTest {
     @Autowired
     private NotesRepository notesRepository;
 
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    private final UUID ownerId = UUID.randomUUID();
+
     private RegularNote regularNote;
 
     @BeforeEach
@@ -43,8 +64,40 @@ public class ArchivedNoteServiceSwitchArchivedTest {
                 .thenReturn(Optional.of("MONGO_TEST"));
 
         regularNote = new RegularNote();
+        regularNote.setOwnerId(ownerId);
         regularNote.setTitle("Title");
         regularNote.setContent("Content");
         regularNote.setCategory(Category.DIARY);
+    }
+
+    @AfterEach
+    void clear() {
+        notesRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("Set Archived True Test")
+    void setArchivedNoteTrue() {
+
+        regularNote.setArchived(false);
+        RegularNote savedNote = notesRepository.save(regularNote);
+
+        ObjectId noteId = savedNote.getId();
+        assertNotNull(noteId);
+
+        assertFalse(savedNote.isArchived());
+
+        assertDoesNotThrow(() -> archivedNotesService.switchArchivedNoteForUser(ownerId, noteId.toHexString()));
+
+        Note updatedNote = mongoTemplate.findOne(
+                Query.query(
+                        Criteria.where("_id").is(noteId)
+                                .and("ownerId").is(ownerId)
+                ),
+                Note.class
+        );
+
+        assertNotNull(updatedNote);
+        assertTrue(updatedNote.isArchived());
     }
 }
