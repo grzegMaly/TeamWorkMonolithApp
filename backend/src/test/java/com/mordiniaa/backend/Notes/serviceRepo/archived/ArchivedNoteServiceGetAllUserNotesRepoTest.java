79f15bb9e713a5aa492a54cc6e85e4ded49c1613
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-11 18:57:03 +0100

    All Notes User One Archived True Test

--- backend/src/test/java/com/mordiniaa/backend/Notes/serviceRepo/archived/ArchivedNoteServiceGetAllUserNotesRepoTest.java
+++ backend/src/test/java/com/mordiniaa/backend/Notes/serviceRepo/archived/ArchivedNoteServiceGetAllUserNotesRepoTest.java
@@ -1,31 +1,46 @@
 package com.mordiniaa.backend.Notes.serviceRepo.archived;
 
+import com.mordiniaa.backend.dto.NoteDto;
+import com.mordiniaa.backend.dto.RegularNoteDto;
 import com.mordiniaa.backend.mappers.notes.NoteMapper;
+import com.mordiniaa.backend.mappers.notes.dtoMappers.DeadlineNoteDtoMapper;
+import com.mordiniaa.backend.mappers.notes.dtoMappers.RegularNoteDtoMapper;
+import com.mordiniaa.backend.models.notes.Note;
 import com.mordiniaa.backend.models.notes.deadline.DeadlineNote;
 import com.mordiniaa.backend.models.notes.regular.Category;
 import com.mordiniaa.backend.models.notes.regular.RegularNote;
 import com.mordiniaa.backend.repositories.mongo.NotesRepository;
 import com.mordiniaa.backend.services.notes.ArchivedNotesServiceImpl;
+import com.mordiniaa.backend.utils.PageResult;
+import org.bson.types.ObjectId;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 @ActiveProfiles("test")
 @DataMongoTest
 @Import({
         NoteMapper.class,
-        ArchivedNotesServiceImpl.class
+        ArchivedNotesServiceImpl.class,
+        RegularNoteDtoMapper.class,
+        DeadlineNoteDtoMapper.class
 })
 public class ArchivedNoteServiceGetAllUserNotesRepoTest {
 
@@ -40,6 +55,8 @@ public class ArchivedNoteServiceGetAllUserNotesRepoTest {
 
     private final UUID ownerIdOne = UUID.randomUUID();
     private final UUID ownerIdTwo = UUID.randomUUID();
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     @BeforeEach
     void setup() {
@@ -95,7 +112,28 @@ public class ArchivedNoteServiceGetAllUserNotesRepoTest {
     }
 
     @Test
-    void test() {
+    @DisplayName("All Notes User One Archived True Test")
+    void getAllArchivedNotesForUserOne() {
 
+        PageResult<List<NoteDto>> pageResult = archivedNotesService.fetchAllArchivedNotes(ownerIdOne, 0, 30);
+        Set<String> dtoIds = pageResult.getData().stream()
+                .map(NoteDto::getId)
+                .collect(Collectors.toSet());
+
+        Query query = new Query();
+        query.addCriteria(
+                Criteria.where("archived").is(true)
+                        .and("ownerId").is(ownerIdOne)
+        ).limit(30);
+
+        Set<String> noteIds = mongoTemplate.find(query, Note.class).stream()
+                .map(Note::getId)
+                .map(ObjectId::toHexString)
+                .collect(Collectors.toSet());
+
+        assertEquals(26, dtoIds.size());
+        assertEquals(dtoIds.size(), noteIds.size());
+
+        assertTrue(noteIds.containsAll(dtoIds));
     }
 }
