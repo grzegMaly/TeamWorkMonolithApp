0dbb30f2d83dcf0374293cb03b8fef48b1e563f1
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-13 22:47:50 +0100

    Task Category Embedded Document

---
+++ backend/src/main/java/com/mordiniaa/backend/models/taskWorkspaces/TaskCategory.java
@@ -0,0 +1,23 @@
+package com.mordiniaa.backend.models.taskWorkspaces;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+
+import java.time.Instant;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class TaskCategory {
+
+    private int position = 0;
+
+    private String categoryName;
+
+    private List<ObjectId> tasks;
+
+    private Instant createdAt;
+}
