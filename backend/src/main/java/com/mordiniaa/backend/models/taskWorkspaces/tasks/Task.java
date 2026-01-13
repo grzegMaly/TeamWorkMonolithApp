f25fe0b8069a27ed92fff881a0043aa6c881bd70
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-13 22:47:31 +0100

    Task Model

---
+++ backend/src/main/java/com/mordiniaa/backend/models/taskWorkspaces/tasks/Task.java
@@ -0,0 +1,40 @@
+package com.mordiniaa.backend.models.taskWorkspaces.tasks;
+
+import com.mordiniaa.backend.models.taskWorkspaces.BoardMember;
+import com.mordiniaa.backend.models.taskWorkspaces.tasks.activity.TaskActivityElement;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Document("tasks")
+public class Task {
+
+    @Id
+    private ObjectId id;
+
+    private String title;
+    private String description;
+
+    private TaskStatus taskStatus;
+    private List<TaskActivityElement> activityElements;
+
+    private UUID createdBy;
+    private List<UUID> assignedTo;
+
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    private Instant deadline;
+}
