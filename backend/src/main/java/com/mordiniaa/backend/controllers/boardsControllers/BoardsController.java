eb2a2737eb68cdd56f7421a0f1f8b32d72e60cd7
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-13 12:45:06 +0100

    Controller Rename

--- backend/src/main/java/com/mordiniaa/backend/controllers/boardsControllers/BoardsController.java
+++ backend/src/main/java/com/mordiniaa/backend/controllers/boardsControllers/BoardsController.java
@@ -1,9 +1,9 @@
-package com.mordiniaa.backend.controllers.tasksControllers;
+package com.mordiniaa.backend.controllers.boardsControllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/tasks")
-public class TasksController {
+@RequestMapping("/api/v1/boards")
+public class BoardsController {
 }
