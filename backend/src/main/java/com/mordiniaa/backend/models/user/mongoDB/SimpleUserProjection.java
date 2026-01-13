1003f967c4ea13ceb65be129b6ea2e3382062a81
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-13 22:45:39 +0100

    User Projection

---
+++ backend/src/main/java/com/mordiniaa/backend/models/user/mongoDB/SimpleUserProjection.java
@@ -0,0 +1,32 @@
+package com.mordiniaa.backend.models.user.mongoDB;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Document("users")
+public class SimpleUserProjection {
+
+    @Id
+    private ObjectId id;
+
+    @Indexed
+    @Field("ownerId")
+    private UUID userId;
+
+    @Field("username")
+    private String username;
+
+    @Field("imageUrl")
+    private String imageUrl;
+}
