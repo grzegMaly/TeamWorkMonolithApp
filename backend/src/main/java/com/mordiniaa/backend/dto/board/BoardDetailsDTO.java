63925f62d9e5a23e0c8337ed73267f136e442959
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-15 00:22:46 +0100

    Board Details DTO Implemented

--- backend/src/main/java/com/mordiniaa/backend/dto/board/BoardDetailsDTO.java
+++ backend/src/main/java/com/mordiniaa/backend/dto/board/BoardDetailsDTO.java
@@ -1,4 +1,36 @@
 package com.mordiniaa.backend.dto.board;
 
+import com.mordiniaa.backend.dto.user.mongodb.UserProjection;
+import com.mordiniaa.backend.models.board.task.TaskStatus;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
 public class BoardDetailsDTO {
+
+    private String boardId;
+    private String boardName;
+    private List<TaskCategoryDTO> taskCategories;
+    private List<UserProjection> members;
+    private Instant createdAt;
+
+    private static class TaskCategoryDTO {
+
+        private int position;
+        private String categoryName;
+        private List<TaskCardDTO> tasks;
+        private Instant createdAt;
+
+        private static class TaskCardDTO {
+
+            private String id;
+            private int positionInCategory;
+            private String title;
+            private String description;
+            private TaskStatus taskStatus;
+            private List<UUID> assignedTo;
+            private Instant deadline;
+        }
+    }
 }
