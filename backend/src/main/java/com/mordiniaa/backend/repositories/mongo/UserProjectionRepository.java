e7b9ecddc74a31c132c42022190047713509394b
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-14 22:22:20 +0100

    Rename In User Mongo Repo

--- backend/src/main/java/com/mordiniaa/backend/repositories/mongo/UserProjectionRepository.java
+++ backend/src/main/java/com/mordiniaa/backend/repositories/mongo/UserProjectionRepository.java
@@ -1,8 +1,8 @@
 package com.mordiniaa.backend.repositories.mongo;
 
-import com.mordiniaa.backend.models.user.mongodb.UserProjection;
+import com.mordiniaa.backend.models.user.mongodb.UserRepresentation;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserProjectionRepository extends MongoRepository<UserProjection, ObjectId> {
+public interface UserProjectionRepository extends MongoRepository<UserRepresentation, ObjectId> {
 }
