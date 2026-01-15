f532645ceaf8729b9dc8a43683ca20146ec14434
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-15 16:39:05 +0100

    Repo Rename

--- backend/src/main/java/com/mordiniaa/backend/repositories/mongo/UserRepresentationRepository.java
+++ backend/src/main/java/com/mordiniaa/backend/repositories/mongo/UserRepresentationRepository.java
@@ -4,5 +4,5 @@ import com.mordiniaa.backend.models.user.mongodb.UserRepresentation;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserProjectionRepository extends MongoRepository<UserRepresentation, ObjectId> {
+public interface UserRepresentationRepository extends MongoRepository<UserRepresentation, ObjectId> {
 }
