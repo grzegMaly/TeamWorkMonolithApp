2b34f1b511a38aaecfa01a7dba647c0f3f24a400
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-16 16:03:52 +0100

    Rename in BoardMember

--- backend/src/main/java/com/mordiniaa/backend/models/board/BoardMember.java
+++ backend/src/main/java/com/mordiniaa/backend/models/board/BoardMember.java
@@ -3,7 +3,7 @@ package com.mordiniaa.backend.models.board;
 import com.mordiniaa.backend.models.board.permissions.CategoryPermissions;
 import com.mordiniaa.backend.models.board.permissions.CommentPermission;
 import com.mordiniaa.backend.models.board.permissions.TaskPermission;
-import com.mordiniaa.backend.models.board.permissions.WorkspacePermission;
+import com.mordiniaa.backend.models.board.permissions.BoardPermission;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -18,7 +18,7 @@ import java.util.UUID;
 public class BoardMember {
 
     private UUID userId;
-    private Set<WorkspacePermission> workspacePermissions = new HashSet<>();
+    private Set<BoardPermission> boardPermissions = new HashSet<>();
     private Set<CategoryPermissions> categoryPermissions = new HashSet<>();
     private Set<TaskPermission> taskPermissions = new HashSet<>();
     private Set<CommentPermission> commentPermissions = new HashSet<>(Set.of(
