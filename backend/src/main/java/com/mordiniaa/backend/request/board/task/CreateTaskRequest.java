9482903297220f2374a4879b915df0320445954e
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-16 16:06:05 +0100

    Task Simple Requests

---
+++ backend/src/main/java/com/mordiniaa/backend/request/board/task/CreateTaskRequest.java
@@ -0,0 +1,31 @@
+package com.mordiniaa.backend.request.board.task;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+
+@Getter
+@Setter
+@NotNull
+public class CreateTaskRequest {
+
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String title;
+
+    @NotBlank
+    @Size(min = 3, max = 512)
+    private String description;
+
+    private List<UUID> assignedTo; //Optional
+
+    @NotNull
+    private Instant deadline;
+}
