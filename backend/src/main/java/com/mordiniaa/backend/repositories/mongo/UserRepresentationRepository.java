d6da55db2f04c43d5bd35553e8166ee85035ffcf
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-16 16:05:06 +0100

    Checking Existence Method Added

--- backend/src/main/java/com/mordiniaa/backend/repositories/mongo/UserRepresentationRepository.java
+++ backend/src/main/java/com/mordiniaa/backend/repositories/mongo/UserRepresentationRepository.java
@@ -4,5 +4,9 @@ import com.mordiniaa.backend.models.user.mongodb.UserRepresentation;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.UUID;
+
 public interface UserRepresentationRepository extends MongoRepository<UserRepresentation, ObjectId> {
+
+    boolean existsUserRepresentationByUserIdAndDeletedFalse(UUID userId);
 }
