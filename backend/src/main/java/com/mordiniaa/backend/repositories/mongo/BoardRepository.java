93eedfe742c25e30e0661038301601816388287f
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-16 16:05:24 +0100

    Query Finding Board With Specified Fields Added

--- backend/src/main/java/com/mordiniaa/backend/repositories/mongo/BoardRepository.java
+++ backend/src/main/java/com/mordiniaa/backend/repositories/mongo/BoardRepository.java
@@ -3,6 +3,21 @@ package com.mordiniaa.backend.repositories.mongo;
 import com.mordiniaa.backend.models.board.Board;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public interface BoardRepository extends MongoRepository<Board, ObjectId> {
+
+    @Query(
+        value = "{'_id': ?0}",
+        fields = """
+                {
+                    'taskCategories': {$elemMatch: {'categoryName': ?1}},
+                    'members': {$elemMatch: {'userId': ?2}}
+                }
+                """
+    )
+    Optional<Board> getBoardByIdWithCategoryAndBoardMember(ObjectId objectId, String categoryName, UUID userId);
 }
