9b9a62d8cc42ac034ebae83752a5aec726d557d4
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-17 16:42:35 +0100

    Board Repo Method With Projection

--- backend/src/main/java/com/mordiniaa/backend/repositories/mongo/BoardRepository.java
+++ backend/src/main/java/com/mordiniaa/backend/repositories/mongo/BoardRepository.java
@@ -2,22 +2,39 @@ package com.mordiniaa.backend.repositories.mongo;
 
 import com.mordiniaa.backend.models.board.Board;
 import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface BoardRepository extends MongoRepository<Board, ObjectId> {
 
-    @Query(
-        value = "{'_id': ?0}",
-        fields = """
-                {
-                    'taskCategories': {$elemMatch: {'categoryName': ?1}},
-                    'members': {$elemMatch: {'userId': ?2}}
-                }
-                """
-    )
+    @Aggregation(pipeline = {
+            "{$match: {_id: ?0}}",
+            "{$match:  {'members.userId': ?2}}",
+            """
+                    {
+                        $project: {
+                                taskCategories: {
+                                            $filter: {
+                                                input: "$taskCategories",
+                                                as: "cat",
+                                                cond: {$eq:  ["$$cat.categoryName", ?1]}}},
+                                members: {
+                                            $map: {
+                                                        input: "$members",
+                                                        as: "m",
+                                                        in: {
+                                                            $cond: [{$eq: ["$$m.userId", ?2]},
+                                                                    "$$m",
+                                                                    {userId: "$$m.userId"}]
+                                                            }
+                                                }
+                                        }
+                                }
+                    }
+                    """
+    })
     Optional<Board> getBoardByIdWithCategoryAndBoardMember(ObjectId objectId, String categoryName, UUID userId);
 }
