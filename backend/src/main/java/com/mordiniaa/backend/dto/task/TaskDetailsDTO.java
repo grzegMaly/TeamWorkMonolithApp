7b7661f600ef0f209241bc9b854d885803acff66
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-19 16:38:10 +0100

    Dto Class Added

--- backend/src/main/java/com/mordiniaa/backend/dto/task/TaskDetailsDTO.java
+++ backend/src/main/java/com/mordiniaa/backend/dto/task/TaskDetailsDTO.java
@@ -1,4 +1,16 @@
 package com.mordiniaa.backend.dto.task;
 
-public class TaskDetailsDTO {
+import com.mordiniaa.backend.models.task.activity.TaskActivityElement;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class TaskDetailsDTO extends TaskShortDto {
+
+    private Set<TaskActivityElement> taskActivityElements;
 }
