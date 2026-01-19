70df884cf8d0e72ed2a85538d1d70bfa8fda2fb3
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-20 00:42:39 +0100

    Category Change Mapper

--- backend/src/main/java/com/mordiniaa/backend/mappers/task/activityMappers/dtoMappers/TaskCategoryChangeDtoMapper.java
+++ backend/src/main/java/com/mordiniaa/backend/mappers/task/activityMappers/dtoMappers/TaskCategoryChangeDtoMapper.java
@@ -1,4 +1,32 @@
 package com.mordiniaa.backend.mappers.task.activityMappers.dtoMappers;
 
-public class TaskCategoryChangeDtoMapper {
+import com.mordiniaa.backend.dto.task.activity.TaskCategoryChangeDto;
+import com.mordiniaa.backend.mappers.User.UserRepresentationMapper;
+import com.mordiniaa.backend.models.task.activity.TaskCategoryChange;
+import com.mordiniaa.backend.models.user.mongodb.UserRepresentation;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TaskCategoryChangeDtoMapper extends AbstractActivityDtoMapper<TaskCategoryChange, TaskCategoryChangeDto> {
+
+    public TaskCategoryChangeDtoMapper(UserRepresentationMapper userRepresentationMapper) {
+        super(userRepresentationMapper);
+    }
+
+    @Override
+    protected TaskCategoryChangeDto toTypedDto(TaskCategoryChange element, UserRepresentation user) {
+
+        TaskCategoryChangeDto.TaskCategoryChangeDtoBuilder<?, ?> builder =
+                TaskCategoryChangeDto.builder();
+        mapBase(element, builder, user);
+        return builder
+                .prevTaskCategoryName(element.getPrevCategory())
+                .nextTaskCategoryName(element.getNextCategory())
+                .build();
+    }
+
+    @Override
+    public Class<TaskCategoryChange> getSupportedType() {
+        return TaskCategoryChange.class;
+    }
 }
