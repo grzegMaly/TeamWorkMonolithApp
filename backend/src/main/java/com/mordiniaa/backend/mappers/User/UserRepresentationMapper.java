40ece2f3a6ca5d4f59bf12a1c8ec13f6a4e46581
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-20 00:42:11 +0100

    UserRepresentation Simple Mapper

--- backend/src/main/java/com/mordiniaa/backend/mappers/User/UserRepresentationMapper.java
+++ backend/src/main/java/com/mordiniaa/backend/mappers/User/UserRepresentationMapper.java
@@ -1,4 +1,17 @@
 package com.mordiniaa.backend.mappers.User;
 
-public class UserMongoMapper {
+import com.mordiniaa.backend.dto.user.mongodb.UserDto;
+import com.mordiniaa.backend.models.user.mongodb.UserRepresentation;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserRepresentationMapper {
+
+    public UserDto toDto(UserRepresentation userRepresentation) {
+        return UserDto.builder()
+                .userId(userRepresentation.getUserId())
+                .username(userRepresentation.getUsername())
+                .imageUrl(userRepresentation.getImageUrl())
+                .build();
+    }
 }
