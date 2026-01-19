0c655643cbe80dd8126e862031321106d14e5aa1
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-19 16:11:49 +0100

    Field Name Fixed

--- backend/src/main/java/com/mordiniaa/backend/models/task/activity/TaskStatusChange.java
+++ backend/src/main/java/com/mordiniaa/backend/models/task/activity/TaskStatusChange.java
@@ -12,6 +12,6 @@ import org.springframework.data.annotation.TypeAlias;
 @NoArgsConstructor
 public class TaskStatusChange extends TaskActivityElement {
 
-    private TaskStatus prevStaus;
+    private TaskStatus prevStatus;
     private TaskStatus nextStatus;
 }
