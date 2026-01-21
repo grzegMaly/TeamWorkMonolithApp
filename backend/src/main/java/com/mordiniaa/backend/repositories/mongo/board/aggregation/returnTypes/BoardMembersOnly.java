41f0a16e71926b13545c39f8bce8cb6173c414ae
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-21 18:19:15 +0100

    Aggregation Return Types

--- backend/src/main/java/com/mordiniaa/backend/repositories/mongo/board/aggregation/returnTypes/BoardMembersOnly.java
+++ backend/src/main/java/com/mordiniaa/backend/repositories/mongo/board/aggregation/returnTypes/BoardMembersOnly.java
@@ -1,6 +1,7 @@
 package com.mordiniaa.backend.repositories.mongo.board.aggregation.returnTypes;
 
 import com.mordiniaa.backend.models.board.BoardMember;
+import com.mordiniaa.backend.models.board.BoardMembers;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,7 +11,7 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
-public class BoardMembersOnly{
+public class BoardMembersOnly implements BoardMembers {
 
     private BoardMember owner;
     private List<BoardMember> members;
--- backend/src/main/java/com/mordiniaa/backend/repositories/mongo/board/aggregation/returnTypes/BoardMembersTasksOnly.java
+++ backend/src/main/java/com/mordiniaa/backend/repositories/mongo/board/aggregation/returnTypes/BoardMembersTasksOnly.java
@@ -1,4 +1,15 @@
 package com.mordiniaa.backend.repositories.mongo.board.aggregation.returnTypes;
 
-public class BoardMembersTasksOnly {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class BoardMembersTasksOnly extends BoardMembersOnly {
+
+    private List<TaskCreatorProjection> tasks;
 }
