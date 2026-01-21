0147b2995e40a477786534fce648f8ebefd6cf3a
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-21 22:29:14 +0100

    Service Added For Mongo User

--- backend/src/main/java/com/mordiniaa/backend/services/notes/user/MongoUserService.java
+++ backend/src/main/java/com/mordiniaa/backend/services/notes/user/MongoUserService.java
@@ -1,4 +1,21 @@
 package com.mordiniaa.backend.services.notes.user;
 
+import com.mordiniaa.backend.repositories.mongo.UserRepresentationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
 public class MongoUserService {
+
+    private final UserRepresentationRepository userRepresentationRepository;
+
+    public void checkUserAvailability(UUID userId) {
+        boolean result = userRepresentationRepository.existsUserRepresentationByUserIdAndDeletedFalse(userId);
+        if (!result) {
+            throw new RuntimeException(); //TODO: Change in Exceptions Section
+        }
+    }
 }
