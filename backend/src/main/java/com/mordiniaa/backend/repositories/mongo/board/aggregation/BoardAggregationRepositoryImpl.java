2b1c5966e8ad59f62e5b2c65ef213582770d6391
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-21 18:19:31 +0100

    Aggregation Repo Method

--- backend/src/main/java/com/mordiniaa/backend/repositories/mongo/board/aggregation/BoardAggregationRepositoryImpl.java
+++ backend/src/main/java/com/mordiniaa/backend/repositories/mongo/board/aggregation/BoardAggregationRepositoryImpl.java
@@ -1,18 +1,20 @@
 package com.mordiniaa.backend.repositories.mongo.board.aggregation;
 
 import com.mordiniaa.backend.repositories.mongo.board.aggregation.returnTypes.BoardMembersOnly;
+import com.mordiniaa.backend.repositories.mongo.board.aggregation.returnTypes.BoardMembersTasksOnly;
 import lombok.RequiredArgsConstructor;
+import org.bson.Document;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.*;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.match;
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.project;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
 
 @Repository
 @RequiredArgsConstructor
@@ -40,8 +42,42 @@ public class BoardAggregationRepositoryImpl implements BoardAggregationRepositor
                 .findFirst();
     }
 
-    public Optional<BoardMembersOnly> findBoardWithSpecifiedMemberOnly(ObjectId boardId, UUID userId, ObjectId taskId) {
+    @Override
+    public Optional<BoardMembersTasksOnly> findBoardWithSpecifiedMemberOnly(ObjectId boardId, UUID userId, ObjectId taskId) {
 
-        return Optional.empty();
+        LookupOperation lookupOperation = LookupOperation.newLookup()
+                .from("tasks")
+                .let(VariableOperators.Let.ExpressionVariable
+                        .newVariable("taskIds")
+                        .forField("$taskCategories.tasks"))
+                .pipeline(
+                        Aggregation.match(
+                                Criteria.expr(() ->
+                                        new Document("$in", List.of("$_id", "$$taskIds"))
+                                )
+                        ),
+                        Aggregation.project("createdBy")
+                )
+                .as("tasks");
+
+
+        Aggregation aggr = Aggregation.newAggregation(
+                match(Criteria.where("_id").is(boardId)),
+                match(new Criteria().orOperator(
+                        Criteria.where("owner.userId").is(userId),
+                        Criteria.where("members.userId").is(userId)
+                )),
+                unwind("taskCategories"),
+                match(Criteria.where("taskCategories.tasks").is(taskId)),
+                lookupOperation,
+                group("_id")
+                        .first("owner").as("owner")
+                        .first("members").as("members")
+                        .first("tasks").as("tasks")
+        );
+        return mongoTemplate.aggregate(aggr, "boards", BoardMembersTasksOnly.class)
+                .getMappedResults()
+                .stream()
+                .findFirst();
     }
 }
