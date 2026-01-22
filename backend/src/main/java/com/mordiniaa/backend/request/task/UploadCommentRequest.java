b8b0358bd775ef2d75c9b3b4125da5320e2bc1fc
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-22 13:35:31 +0100

    Upload Comment Request

--- backend/src/main/java/com/mordiniaa/backend/request/task/UploadCommentRequest.java
+++ backend/src/main/java/com/mordiniaa/backend/request/task/UploadCommentRequest.java
@@ -1,4 +1,25 @@
 package com.mordiniaa.backend.request.task;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class UploadCommentRequest {
+
+    private UUID commentId;
+
+    @NotBlank
+    @Size(min = 3, max = 256)
+    private String comment;
+
+    public void setContent(String comment) {
+        this.comment = comment.trim();
+    }
 }
