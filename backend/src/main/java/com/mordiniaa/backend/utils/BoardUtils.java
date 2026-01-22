31244ae7f4403058cb546c0d282c85a8504c21c5
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-22 11:58:44 +0100

    Utility Board Method Moved To Separate Class

--- backend/src/main/java/com/mordiniaa/backend/utils/BoardUtils.java
+++ backend/src/main/java/com/mordiniaa/backend/utils/BoardUtils.java
@@ -1,4 +1,20 @@
 package com.mordiniaa.backend.utils;
 
+import com.mordiniaa.backend.models.board.BoardMember;
+import com.mordiniaa.backend.models.board.BoardMembers;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
 public class BoardUtils {
+
+    public BoardMember getBoardMember(BoardMembers board, UUID userId) {
+        if (board.getOwner().getUserId().equals(userId)) {
+            return board.getOwner();
+        } else {
+            return board.getMembers().stream().filter(bm -> bm.getUserId().equals(userId))
+                    .findFirst().orElseThrow(RuntimeException::new);
+        }
+    }
 }
