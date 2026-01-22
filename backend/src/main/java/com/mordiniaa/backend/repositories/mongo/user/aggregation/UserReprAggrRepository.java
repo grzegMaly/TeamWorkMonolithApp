3b5f1b217f4f726dd0dd4c0328bb5ea39d0c82d3
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-22 22:37:33 +0100

    Custom Repo Added And Method Implemented

--- backend/src/main/java/com/mordiniaa/backend/repositories/mongo/user/aggregation/UserReprAggrRepository.java
+++ backend/src/main/java/com/mordiniaa/backend/repositories/mongo/user/aggregation/UserReprAggrRepository.java
@@ -1,4 +1,8 @@
 package com.mordiniaa.backend.repositories.mongo.user.aggregation;
 
+import java.util.Set;
+import java.util.UUID;
+
 public interface UserReprAggrRepository {
+    boolean allUsersAvailable(Set<UUID> userIds);
 }
--- backend/src/main/java/com/mordiniaa/backend/repositories/mongo/user/aggregation/UserReprAggrRepositoryImpl.java
+++ backend/src/main/java/com/mordiniaa/backend/repositories/mongo/user/aggregation/UserReprAggrRepositoryImpl.java
@@ -1,4 +1,33 @@
 package com.mordiniaa.backend.repositories.mongo.user.aggregation;
 
+import com.mordiniaa.backend.models.user.mongodb.UserRepresentation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Repository
+@RequiredArgsConstructor
 public class UserReprAggrRepositoryImpl implements UserReprAggrRepository {
+
+    private final MongoTemplate mongoTemplate;
+
+    @Override
+    public boolean allUsersAvailable(Set<UUID> userIds) {
+
+        if (userIds == null || userIds.isEmpty())
+            throw new RuntimeException(); // TODO:
+
+        Query query = Query.query(
+                Criteria.where("_id").in(userIds)
+                        .and("deleted").is(false)
+        );
+
+        long count = mongoTemplate.count(query, UserRepresentation.class);
+        return count == userIds.size();
+    }
 }
