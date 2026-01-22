8d86378242715b54ccfcbf59955678ecd7e88a2e
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-22 13:09:56 +0100

    Task Comment Constructor

--- backend/src/main/java/com/mordiniaa/backend/models/task/activity/TaskComment.java
+++ backend/src/main/java/com/mordiniaa/backend/models/task/activity/TaskComment.java
@@ -16,4 +16,9 @@ public class TaskComment extends TaskActivityElement {
     private UUID commentId;
     private String comment;
     private boolean updated = false;
+
+    public TaskComment(UUID user) {
+        super(user);
+        this.commentId = UUID.randomUUID();
+    }
 }
