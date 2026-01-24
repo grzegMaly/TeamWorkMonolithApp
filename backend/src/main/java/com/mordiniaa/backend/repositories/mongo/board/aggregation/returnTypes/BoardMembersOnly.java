1b7c7e962aaf5e3960293ae60f4e025e85cc73c1
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-24 16:53:16 +0100

    Class Extended

--- backend/src/main/java/com/mordiniaa/backend/repositories/mongo/board/aggregation/returnTypes/BoardMembersOnly.java
+++ backend/src/main/java/com/mordiniaa/backend/repositories/mongo/board/aggregation/returnTypes/BoardMembersOnly.java
@@ -2,17 +2,20 @@ package com.mordiniaa.backend.repositories.mongo.board.aggregation.returnTypes;
 
 import com.mordiniaa.backend.models.board.BoardMember;
 import com.mordiniaa.backend.models.board.BoardMembers;
+import com.mordiniaa.backend.models.board.BoardTemplate;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.bson.types.ObjectId;
 
 import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
-public class BoardMembersOnly implements BoardMembers {
+public class BoardMembersOnly implements BoardMembers, BoardTemplate {
 
+    private ObjectId id;
     private BoardMember owner;
     private List<BoardMember> members;
 }
