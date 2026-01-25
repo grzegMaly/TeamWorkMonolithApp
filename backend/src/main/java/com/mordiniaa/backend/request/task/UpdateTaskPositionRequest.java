423ecafbf98204794c1d2e7bcefe5fca2caa7c1d
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-26 00:00:47 +0100

    sdcvv

--- backend/src/main/java/com/mordiniaa/backend/request/task/UpdateTaskPositionRequest.java
+++ backend/src/main/java/com/mordiniaa/backend/request/task/UpdateTaskPositionRequest.java
@@ -1,6 +1,5 @@
 package com.mordiniaa.backend.request.task;
 
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Getter;
