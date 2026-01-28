88f7001b43b435def73d0df2298c95465d197720
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-01-29 00:49:04 +0100

    Board Not Found

--- backend/src/test/java/com/mordiniaa/backend/services/task/TaskManagementRemoveUserFromTaskRepoTest.java
+++ backend/src/test/java/com/mordiniaa/backend/services/task/TaskManagementRemoveUserFromTaskRepoTest.java
@@ -306,6 +306,22 @@ public class TaskManagementRemoveUserFromTaskRepoTest {
         assertTrue(task.getAssignedTo().contains(member1Id));
     }
 
+    @Test
+    @Order(5)
+    @DisplayName("Board Not Found")
+    void boardNotFoundTest() {
+
+        String bId = ObjectId.get().toHexString();
+        String tId = task2.getId().toHexString();
+
+        assertThrows(RuntimeException.class, () -> taskManagementService.removeUserFromTask(
+                member1Id,
+                member2Id,
+                bId,
+                tId
+        ));
+    }
+
     private void setAssignmentPermissionForMember(BoardMember member) {
 
         member.setBoardPermissions(Set.of(BoardPermission.VIEW_BOARD));
