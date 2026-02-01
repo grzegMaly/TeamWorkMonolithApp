84b4b7c44038db380a817a799c44580793d9b458
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-01 22:48:33 +0100

    Board Service Fro Owner

---
+++ backend/src/main/java/com/mordiniaa/backend/services/board/admin/BoardOwnerService.java
@@ -0,0 +1,31 @@
+package com.mordiniaa.backend.services.board.admin;
+
+import com.mordiniaa.backend.models.board.Board;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BoardOwnerService {
+
+    public Board createBoard() {
+
+        return null;
+    }
+
+    public void addUserToBoard() {
+
+    }
+
+    public void removeUserFromBoard() {
+
+    }
+
+    public void updateBoardDetails() {
+
+    }
+
+    public void deleteBoard() {
+
+    }
+}
