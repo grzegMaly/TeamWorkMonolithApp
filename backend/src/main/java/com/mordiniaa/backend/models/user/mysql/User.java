c5a63fc4e9bd39f48ad0655b55a2a8622150c8e3
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-01 23:32:07 +0100

    User Model

--- backend/src/main/java/com/mordiniaa/backend/models/user/mysql/User.java
+++ backend/src/main/java/com/mordiniaa/backend/models/user/mysql/User.java
@@ -1,4 +1,75 @@
 package com.mordiniaa.backend.models.user.mysql;
 
+import com.mordiniaa.backend.models.team.Team;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "User")
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
+    private UUID userId;
+
+    @Column(name = "first_name", nullable = false, length = 20)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 20)
+    private String lastName;
+
+    @Column(name = "username", nullable = false, length = 20)
+    private String username;
+
+    @Column(name = "email", nullable = false, length = 50)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 60)
+    private String password;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+    @JoinColumn(name = "role_id", referencedColumnName = "role_id", nullable = false)
+    private Role role;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private Set<Address> addresses = new HashSet<>();
+
+    @ManyToMany(mappedBy = "teamMembers")
+    private Set<Team> teams = new HashSet<>();
+
+    @OneToMany(mappedBy = "manager", fetch = FetchType.LAZY)
+    private Set<Team> ownedTeams = new HashSet<>();
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false, nullable = false, length = 20)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "updated_by", length = 20)
+    private String updatedBy;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
