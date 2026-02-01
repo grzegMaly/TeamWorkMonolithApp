843babd5bcad2b3a8967cdc797282913fa0f833b
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-01 22:48:27 +0100

    Board Task Category For Owner Template

---
+++ backend/src/main/java/com/mordiniaa/backend/services/board/admin/BoardOwnerTaskCategoryService.java
@@ -0,0 +1,22 @@
+package com.mordiniaa.backend.services.board.admin;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BoardOwnerTaskCategoryService {
+
+    public void createTaskCategory() {
+    }
+
+    public void renameTaskCategory() {
+
+    }
+
+    public void deleteTaskCategory() {
+    }
+
+    public void reorderTaskCategories() {
+    }
+}
