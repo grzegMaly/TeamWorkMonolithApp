76be369769e2cb883c24c50ff3f1a8f4f8be5d3b
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-01 23:31:51 +0100

    Team Model

--- backend/src/main/java/com/mordiniaa/backend/models/team/Team.java
+++ backend/src/main/java/com/mordiniaa/backend/models/team/Team.java
@@ -1,4 +1,62 @@
-package com.mordiniaa.backend.models;
+package com.mordiniaa.backend.models.team;
 
+import com.mordiniaa.backend.models.user.mysql.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity(name = "Team")
+@Table(name = "teams")
 public class Team {
+
+    @Id
+    @Column(name = "team_id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID teamId;
+
+    @Column(name = "team_name", nullable = false, length = 40)
+    private String teamName;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+    @JoinColumn(name = "manager_id", referencedColumnName = "user_id")
+    private User manager;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "teams_users",
+            joinColumns = @JoinColumn(name = "team_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> teamMembers = new HashSet<>();
+
+    @Column(name = "active")
+    private boolean active = true;
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false, nullable = false, length = 20)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "updated_by", length = 20)
+    private String updatedBy;
+
+    @Column(name = "created_at", updatable = false, nullable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
