e916ecd17a6d64205f9573ad8ba9e97445367fe5
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-02 17:05:19 +0100

    Aggregaton Methods Added

--- backend/src/main/java/com/mordiniaa/backend/repositories/mongo/board/aggregation/BoardAggregationRepository.java
+++ backend/src/main/java/com/mordiniaa/backend/repositories/mongo/board/aggregation/BoardAggregationRepository.java
@@ -1,11 +1,14 @@
 package com.mordiniaa.backend.repositories.mongo.board.aggregation;
 
+import com.mordiniaa.backend.models.board.Board;
+import com.mordiniaa.backend.repositories.mongo.board.aggregation.returnTypes.BoardFull;
 import com.mordiniaa.backend.repositories.mongo.board.aggregation.returnTypes.BoardMembersOnly;
 import com.mordiniaa.backend.repositories.mongo.board.aggregation.returnTypes.BoardMembersTasksOnly;
 import com.mordiniaa.backend.repositories.mongo.board.aggregation.returnTypes.BoardWithTaskCategories;
 import org.bson.types.ObjectId;
 
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
 public interface BoardAggregationRepository {
@@ -15,4 +18,8 @@ public interface BoardAggregationRepository {
     Optional<BoardMembersTasksOnly> findBoardForTaskWithCategory(ObjectId boardId, UUID userId, ObjectId taskId);
 
     Optional<BoardWithTaskCategories> findBoardForTaskWithCategories(ObjectId boardId, UUID userId, ObjectId taskId);
+
+    Set<Board> findAllBoardsForUserByUserIdAndTeamId(UUID userId, UUID teamId);
+
+    Optional<BoardFull> findBoardWithTasksByUserIdAndBoardIdAndTeamId(UUID userId, ObjectId boardId, UUID teamId);
 }
