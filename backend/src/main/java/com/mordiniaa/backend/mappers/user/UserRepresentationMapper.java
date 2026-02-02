7378ee4e06747271f6319c5e7787b0422640060b
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-02 17:04:04 +0100

    Class Changed

--- backend/src/main/java/com/mordiniaa/backend/mappers/user/UserRepresentationMapper.java
+++ backend/src/main/java/com/mordiniaa/backend/mappers/user/UserRepresentationMapper.java
@@ -1,14 +1,14 @@
 package com.mordiniaa.backend.mappers.user;
 
-import com.mordiniaa.backend.dto.user.mongodb.UserDto;
+import com.mordiniaa.backend.dto.user.mongodb.MongoUserDto;
 import com.mordiniaa.backend.models.user.mongodb.UserRepresentation;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserRepresentationMapper {
 
-    public UserDto toDto(UserRepresentation userRepresentation) {
-        return UserDto.builder()
+    public MongoUserDto toDto(UserRepresentation userRepresentation) {
+        return MongoUserDto.builder()
                 .userId(userRepresentation.getUserId())
                 .username(userRepresentation.getUsername())
                 .imageUrl(userRepresentation.getImageUrl())
