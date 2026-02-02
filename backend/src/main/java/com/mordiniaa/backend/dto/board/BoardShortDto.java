027ca2c9368a750968107c1130ddb65a8d51ea5b
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-02 17:03:23 +0100

    Short Dto Class Changed

--- backend/src/main/java/com/mordiniaa/backend/dto/board/BoardShortDto.java
+++ backend/src/main/java/com/mordiniaa/backend/dto/board/BoardShortDto.java
@@ -1,4 +1,14 @@
 package com.mordiniaa.backend.dto.board;
 
-public class BoardSchortDto {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class BoardShortDto {
+
+    private String boardId;
+    private String boardName;
 }
