d98d2f3d1f35d98a5b333fcba47fa98b43f0589e
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-03 22:35:03 +0100

    Requests

--- backend/src/main/java/com/mordiniaa/backend/request/board/BoardCreationRequest.java
+++ backend/src/main/java/com/mordiniaa/backend/request/board/BoardCreationRequest.java
@@ -1,4 +1,16 @@
 package com.mordiniaa.backend.request.board;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class BoardCreationRequest {
+
+    private UUID teamId;
+    private String boardName;
 }
--- backend/src/main/java/com/mordiniaa/backend/request/board/PermissionsRequest.java
+++ backend/src/main/java/com/mordiniaa/backend/request/board/PermissionsRequest.java
@@ -1,4 +1,22 @@
 package com.mordiniaa.backend.request.board;
 
+import com.mordiniaa.backend.models.board.permissions.BoardPermission;
+import com.mordiniaa.backend.models.board.permissions.CategoryPermissions;
+import com.mordiniaa.backend.models.board.permissions.CommentPermission;
+import com.mordiniaa.backend.models.board.permissions.TaskPermission;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class PermissionsRequest {
+
+    private Set<BoardPermission> boardPermissions;
+    private Set<CategoryPermissions> categoryPermissions;
+    private Set<TaskPermission> taskPermissions;
+    private Set<CommentPermission> commentPermissions;
 }
