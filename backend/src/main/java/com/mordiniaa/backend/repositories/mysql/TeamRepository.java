4d0f32fa90f5bbf8163facfaefebb59be3b3a36c
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-04 00:39:55 +0100

    Custom Method

--- backend/src/main/java/com/mordiniaa/backend/repositories/mysql/TeamRepository.java
+++ backend/src/main/java/com/mordiniaa/backend/repositories/mysql/TeamRepository.java
@@ -2,6 +2,7 @@ package com.mordiniaa.backend.repositories.mysql;
 
 import com.mordiniaa.backend.models.team.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +11,7 @@ import java.util.UUID;
 @Repository
 public interface TeamRepository extends JpaRepository<Team, UUID> {
     boolean existsTeamByTeamIdAndManager_UserId(UUID teamId, UUID managerUserId);
+
+    @Query("select count(u) > 0 from Team t join t.teamMembers u where t.teamId = :teamid and u.userId = :userId")
+    boolean existsUserInTeam(UUID teamId, UUID userId);
 }
