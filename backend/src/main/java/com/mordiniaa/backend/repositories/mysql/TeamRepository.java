92af084482017d8465d22ff9c3bdc6221afa68be
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-03 22:34:55 +0100

    Team Repository

--- backend/src/main/java/com/mordiniaa/backend/repositories/mysql/TeamRepository.java
+++ backend/src/main/java/com/mordiniaa/backend/repositories/mysql/TeamRepository.java
@@ -1,4 +1,13 @@
 package com.mordiniaa.backend.repositories.mysql;
 
-public interface TeamRepository {
+import com.mordiniaa.backend.models.team.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface TeamRepository extends JpaRepository<Team, UUID> {
+    boolean existsTeamByTeamIdAndManager_UserId(UUID teamId, UUID managerUserId);
 }
