91446f4a4989c5e3a7490dc1ba586fdb9aa7a70b
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-04 14:05:34 +0100

    Indexes Changed

--- backend/src/main/resources/mongodbFiles/3/2boardIndexes.mongodb.js
+++ backend/src/main/resources/mongodbFiles/3/2boardIndexes.mongodb.js
@@ -3,13 +3,32 @@ use team-work-test
 db.auth("team-work-tester", "password")
 
 db.boards.createIndexes([
-    {
-        owner: 1
-    },
-    {
-        boardName: "text"
-    },
-    {
-        teamId: 1
-    }
+{
+    _id: 1,
+    "owner.userId": 1,
+    "members.userId": 1
+},
+{
+  _id: 1,
+  "owner.userId": 1,
+  "members.userId": 1,
+  "taskCategories.tasks": 1
+},
+{
+    _id: 1,
+    "owner.userId": 1,
+    deleted: 1,
+    archived: 1
+},
+{
+  teamId: 1,
+  "owner.userId": 1,
+  "members.userId": 1
+},
+{
+  _id: 1,
+  teamId: 1,
+  "owner.userId": 1,
+  "members.userId": 1
+}
 ])
