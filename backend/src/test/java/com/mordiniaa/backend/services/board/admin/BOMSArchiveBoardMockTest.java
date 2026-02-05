f5f4328a19e03f5d1b1af8ecbc53f729344b349a
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-06 00:45:24 +0100

    Test Class Setup

---
+++ backend/src/test/java/com/mordiniaa/backend/services/board/admin/BOMSArchiveBoardMockTest.java
@@ -0,0 +1,40 @@
+package com.mordiniaa.backend.services.board.admin;
+
+import com.mongodb.client.result.UpdateResult;
+import com.mordiniaa.backend.models.board.Board;
+import com.mordiniaa.backend.services.user.MongoUserService;
+import com.mordiniaa.backend.utils.MongoIdUtils;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class BOMSArchiveBoardMockTest {
+
+    @InjectMocks
+    private BoardOwnerManagementService managementService;
+
+    @Mock
+    private MongoUserService mongoUserService;
+
+    @Mock
+    private MongoIdUtils mongoIdUtils;
+
+    @Mock
+    private MongoTemplate mongoTemplate;
+}
