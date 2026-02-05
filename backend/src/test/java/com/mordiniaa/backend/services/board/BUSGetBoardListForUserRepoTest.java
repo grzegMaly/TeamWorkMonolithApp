43212bb8d3e76edcf20733f6d4166277e13f0973
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-05 18:00:04 +0100

    Get Board List For User Invalid Test

--- backend/src/test/java/com/mordiniaa/backend/services/board/BUSGetBoardListForUserRepoTest.java
+++ backend/src/test/java/com/mordiniaa/backend/services/board/BUSGetBoardListForUserRepoTest.java
@@ -9,7 +9,9 @@ import com.mordiniaa.backend.repositories.mongo.TaskRepository;
 import com.mordiniaa.backend.repositories.mongo.board.BoardRepository;
 import com.mordiniaa.backend.repositories.mongo.board.aggregation.BoardAggregationRepositoryImpl;
 import com.mordiniaa.backend.repositories.mongo.user.UserRepresentationRepository;
+import com.mordiniaa.backend.services.user.MongoUserService;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,7 @@ import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -49,6 +52,35 @@ public class BUSGetBoardListForUserRepoTest {
     private final UUID ownerId = UUID.randomUUID();
     private final UUID teamId = UUID.randomUUID();
 
+    private Board board1;
+    private Board board2;
+    @Autowired
+    private MongoUserService mongoUserService;
+
+    @BeforeEach
+    void setup() {
+        UserRepresentation user = new UserRepresentation();
+        user.setUserId(ownerId);
+        user.setUsername("Username");
+        user.setImageUrl("https://random123.com");
+        userRepresentationRepository.save(user);
+
+        board1 = new Board();
+        board1.setBoardName("Board 1");
+        board1.setOwner(new BoardMember(ownerId));
+        board1.setTeamId(teamId);
+        board1.setUpdatedAt(Instant.now().minus(2, ChronoUnit.DAYS).truncatedTo(ChronoUnit.MILLIS));
+        board1 = boardRepository.save(board1);
+
+        board2 = new Board();
+        board2.setBoardName("Board 2");
+        board2.setOwner(new BoardMember(ownerId));
+        board2.setTeamId(teamId);
+        board2.setUpdatedAt(Instant.now().minus(1, ChronoUnit.DAYS).truncatedTo(ChronoUnit.MILLIS));
+        board2 = boardRepository.save(board2);
+
+    }
+
     @AfterEach
     void clear() {
         boardRepository.deleteAll();
@@ -60,31 +92,17 @@ public class BUSGetBoardListForUserRepoTest {
     @DisplayName("Get Board List For User Valid Test")
     void getBoardListForUserValidTest() {
 
-        UserRepresentation user = new UserRepresentation();
-        user.setUserId(ownerId);
-        user.setUsername("Username");
-        user.setImageUrl("https://random123.com");
-        userRepresentationRepository.save(user);
-
-        Board board1 = new Board();
-        board1.setBoardName("Board 1");
-        board1.setOwner(new BoardMember(ownerId));
-        board1.setTeamId(teamId);
-        board1.setUpdatedAt(Instant.now().minus(2, ChronoUnit.DAYS).truncatedTo(ChronoUnit.MILLIS));
-
-        Board board2 = new Board();
-        board2.setBoardName("Board 2");
-        board2.setOwner(new BoardMember(ownerId));
-        board2.setTeamId(teamId);
-        board2.setUpdatedAt(Instant.now().minus(1, ChronoUnit.DAYS).truncatedTo(ChronoUnit.MILLIS));
-
-        boardRepository.saveAll(List.of(board1, board2));
-
         List<BoardShortDto> boards = boardUserService.getBoardListForUser(ownerId, teamId);
         assertFalse(boards.isEmpty());
 
         assertThat(boards)
                 .extracting(BoardShortDto::getBoardName)
                 .containsExactly(board2.getBoardName(), board1.getBoardName());
+    }
+
+    @Test
+    @DisplayName("Get Board List For User Invalid Test")
+    void getBoardListForUserInvalidTest() {
+        assertThrows(RuntimeException.class, () -> boardUserService.getBoardListForUser(UUID.randomUUID(), teamId));
     }
 }
