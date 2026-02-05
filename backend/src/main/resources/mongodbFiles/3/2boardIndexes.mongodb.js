7b54c635e07c69ef13781c34c7d21557b38c996c
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-05 12:29:59 +0100

    Index Fix

--- backend/src/main/resources/mongodbFiles/3/2boardIndexes.mongodb.js
+++ backend/src/main/resources/mongodbFiles/3/2boardIndexes.mongodb.js
@@ -2,33 +2,13 @@ use team-work-test
 
 db.auth("team-work-tester", "password")
 
-db.boards.createIndexes([
-{
-    _id: 1,
-    "owner.userId": 1,
-    "members.userId": 1
-},
-{
-  _id: 1,
-  "owner.userId": 1,
+db.boards.createIndex({
   "members.userId": 1,
-  "taskCategories.tasks": 1
-},
-{
-    _id: 1,
-    "owner.userId": 1,
-    deleted: 1,
-    archived: 1
-},
-{
-  teamId: 1,
+  teamId: 1
+})
+
+db.boards.createIndex({
   "owner.userId": 1,
-  "members.userId": 1
-},
-{
-  _id: 1,
-  teamId: 1,
-  "owner.userId": 1,
-  "members.userId": 1
-}
-])
+  deleted: 1,
+  archived: 1
+})
