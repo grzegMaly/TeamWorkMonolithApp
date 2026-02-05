d3adb302f586f5c258de30425cf3bfa4462a9270
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-05 15:29:11 +0100

    Get Board List For User Mock Test

---
+++ backend/src/test/java/com/mordiniaa/backend/services/board/BoardUserServiceMockTest.java
@@ -0,0 +1,94 @@
+package com.mordiniaa.backend.services.board;
+
+import com.mordiniaa.backend.dto.board.BoardShortDto;
+import com.mordiniaa.backend.mappers.board.BoardMapper;
+import com.mordiniaa.backend.models.board.Board;
+import com.mordiniaa.backend.repositories.mongo.board.aggregation.BoardAggregationRepositoryImpl;
+import com.mordiniaa.backend.services.user.MongoUserService;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class BoardUserServiceMockTest {
+
+    @InjectMocks
+    private BoardUserService boardUserService;
+
+    @Mock
+    private MongoUserService mongoUserService;
+
+    @Mock
+    private BoardAggregationRepositoryImpl boardAggregationRepository;
+
+    @Mock
+    private BoardMapper boardMapper;
+
+    private final UUID userId = UUID.randomUUID();
+    private final UUID teamId = UUID.randomUUID();
+
+    @Test
+    @DisplayName("Get Board List For User")
+    void getBoardListForUser() {
+
+        doNothing()
+                .when(mongoUserService)
+                .checkUserAvailability(userId);
+
+        Board board1 = new Board();
+        board1.setId(ObjectId.get());
+        board1.setBoardName("Board1");
+        board1.setUpdatedAt(Instant.now().minus(3, ChronoUnit.DAYS));
+
+        Board board2 = new Board();
+        board2.setId(ObjectId.get());
+        board2.setBoardName("Board2");
+        board2.setUpdatedAt(Instant.now().minus(1, ChronoUnit.DAYS));
+
+        Board board3 = new Board();
+        board3.setId(ObjectId.get());
+        board3.setBoardName("Board3");
+        board3.setUpdatedAt(Instant.now().minus(2, ChronoUnit.DAYS));
+
+        when(boardAggregationRepository.findAllBoardsForUserByUserIdAndTeamId(userId, teamId))
+                .thenReturn(Set.of(board1, board2, board3));
+
+        BoardShortDto dto1 = new BoardShortDto();
+        dto1.setBoardId(board1.getId().toHexString());
+        dto1.setBoardName(board1.getBoardName());
+
+        BoardShortDto dto2 = new BoardShortDto();
+        dto2.setBoardId(board2.getId().toHexString());
+        dto2.setBoardName(board2.getBoardName());
+
+        BoardShortDto dto3 = new BoardShortDto();
+        dto3.setBoardId(board3.getId().toHexString());
+        dto3.setBoardName(board3.getBoardName());
+
+        when(boardMapper.toShortDto(board1)).thenReturn(dto1);
+        when(boardMapper.toShortDto(board2)).thenReturn(dto2);
+        when(boardMapper.toShortDto(board3)).thenReturn(dto3);
+
+        List<BoardShortDto> result = boardUserService.getBoardListForUser(userId, teamId);
+        assertThat(result)
+                .extracting(BoardShortDto::getBoardName)
+                .containsExactly(board2.getBoardName(), board3.getBoardName(), board1.getBoardName());
+
+        verify(mongoUserService, times(1)).checkUserAvailability(userId);
+        verify(boardAggregationRepository, times(1)).findAllBoardsForUserByUserIdAndTeamId(userId, teamId);
+    }
+}
