6ea7be4478b2b307b12bd10a7a2301e464e852e8
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-06 23:37:32 +0100

    Test Setup

---
+++ backend/src/test/java/com/mordiniaa/backend/services/board/admin/BoardUserServiceCreateBoardMockTest.java
@@ -0,0 +1,47 @@
+package com.mordiniaa.backend.services.board.admin;
+
+import com.mordiniaa.backend.dto.board.BoardDetailsDto;
+import com.mordiniaa.backend.mappers.board.BoardMapper;
+import com.mordiniaa.backend.models.board.Board;
+import com.mordiniaa.backend.repositories.mongo.board.BoardRepository;
+import com.mordiniaa.backend.repositories.mongo.board.aggregation.BoardAggregationRepositoryImpl;
+import com.mordiniaa.backend.repositories.mongo.board.aggregation.returnTypes.BoardFull;
+import com.mordiniaa.backend.repositories.mysql.TeamRepository;
+import com.mordiniaa.backend.request.board.BoardCreationRequest;
+import com.mordiniaa.backend.services.board.BoardUserService;
+import com.mordiniaa.backend.services.user.MongoUserService;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class BoardUserServiceCreateBoardMockTest {
+
+    @InjectMocks
+    private BoardOwnerService boardOwnerService;
+
+    @Mock
+    private BoardMapper boardMapper;
+
+    @Mock
+    private MongoUserService mongoUserService;
+
+    @Mock
+    private TeamRepository teamRepository;
+
+    @Mock
+    private BoardRepository boardRepository;
+
+    @Mock
+    private BoardAggregationRepositoryImpl boardAggregationRepository;
+}
