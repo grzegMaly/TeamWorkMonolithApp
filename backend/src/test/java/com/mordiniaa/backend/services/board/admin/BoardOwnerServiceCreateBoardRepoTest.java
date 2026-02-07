7112eed6a8b9fd5a1360e6df712f83572e51e8c7
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-07 14:21:11 +0100

    Create Board Valid Test

--- backend/src/test/java/com/mordiniaa/backend/services/board/admin/BoardOwnerServiceCreateBoardRepoTest.java
+++ backend/src/test/java/com/mordiniaa/backend/services/board/admin/BoardOwnerServiceCreateBoardRepoTest.java
@@ -1,6 +1,6 @@
 package com.mordiniaa.backend.services.board.admin;
 
-import com.mordiniaa.backend.models.board.Board;
+import com.mordiniaa.backend.dto.board.BoardDetailsDto;
 import com.mordiniaa.backend.models.team.Team;
 import com.mordiniaa.backend.models.user.mongodb.UserRepresentation;
 import com.mordiniaa.backend.models.user.mysql.AppRole;
@@ -8,6 +8,7 @@ import com.mordiniaa.backend.models.user.mysql.Role;
 import com.mordiniaa.backend.models.user.mysql.User;
 import com.mordiniaa.backend.repositories.mongo.board.BoardRepository;
 import com.mordiniaa.backend.repositories.mongo.user.UserRepresentationRepository;
+import com.mordiniaa.backend.repositories.mysql.RoleRepository;
 import com.mordiniaa.backend.repositories.mysql.TeamRepository;
 import com.mordiniaa.backend.repositories.mysql.UserRepository;
 import com.mordiniaa.backend.request.board.BoardCreationRequest;
@@ -16,7 +17,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -36,15 +39,21 @@ public class BoardOwnerServiceCreateBoardRepoTest {
     private UserRepresentation ownerUser;
 
     private Team team;
+
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private RoleRepository roleRepository;
 
     @BeforeEach
     void setup() {
 
+        Role managerRole = new Role(AppRole.ROLE_MANAGER);
+        managerRole = roleRepository.save(managerRole);
+
         User user = new User();
         user.setUsername("Username");
-        user.setRole(new Role(AppRole.ROLE_MANAGER));
+        user.setRole(managerRole);
         user.setEmail("email@gmail.com");
         user.setFirstName("FirstName");
         user.setLastName("LastName");
@@ -65,8 +74,24 @@ public class BoardOwnerServiceCreateBoardRepoTest {
 
     @AfterEach
     void clear() {
-        userRepository.deleteAll();
         boardRepository.deleteAll();
+        userRepresentationRepository.deleteAll();
         teamRepository.deleteAll();
+        userRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("Create Board Valid Test")
+    void createBoardValidTest() {
+
+        BoardCreationRequest boardCreationRequest = new BoardCreationRequest();
+        boardCreationRequest.setBoardName("BoardName");
+        boardCreationRequest.setTeamId(team.getTeamId());
+
+        BoardDetailsDto boardDetailsDto = boardOwnerService.createBoard(ownerUser.getUserId(), boardCreationRequest);
+        assertNotNull(boardDetailsDto);
+
+        assertEquals(boardCreationRequest.getBoardName(), boardDetailsDto.getBoardName());
+        assertEquals(ownerUser.getUserId(), boardDetailsDto.getOwner().getUserId());
     }
 }
