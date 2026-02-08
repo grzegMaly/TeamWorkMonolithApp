ea6e767e24578bcaa6cab99c05a68bfefbe79b6f
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-09 00:24:36 +0100

    Service Method

--- backend/src/main/java/com/mordiniaa/backend/services/user/UserService.java
+++ backend/src/main/java/com/mordiniaa/backend/services/user/UserService.java
@@ -1,14 +1,24 @@
 package com.mordiniaa.backend.services.user;
 
+import com.mordiniaa.backend.models.user.mysql.User;
 import com.mordiniaa.backend.repositories.mongo.user.UserRepresentationRepository;
+import com.mordiniaa.backend.repositories.mysql.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
+    private final UserRepository userRepository;
     private UserRepresentationRepository userRepresentationRepository;
+
+    public User getUser(UUID userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(RuntimeException::new); // TODO: Change In Exceptions Section
+    }
 
     public void addProfileImage() {
 
