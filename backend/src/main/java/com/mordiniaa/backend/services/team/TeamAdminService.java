3d946366634afd197fe769d3d70edc5dc2eb602b
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-08 23:58:48 +0100

    Create Team Method Implemented

--- backend/src/main/java/com/mordiniaa/backend/services/team/TeamAdminService.java
+++ backend/src/main/java/com/mordiniaa/backend/services/team/TeamAdminService.java
@@ -1,30 +1,55 @@
 package com.mordiniaa.backend.services.team;
 
+import com.mordiniaa.backend.models.team.Team;
+import com.mordiniaa.backend.repositories.mysql.TeamRepository;
+import com.mordiniaa.backend.request.team.TeamCreationRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class TeamAdminService {
 
-    public void createTeam() {
+    private final TeamRepository teamRepository;
 
+    //Protected On Method Lvl For ADMIN
+    @Transactional
+//    @PreAuthorize("hasRole('ADMIN')") TODO: In Future
+    public void createTeam(TeamCreationRequest teamCreationRequest) {
+
+        String teamName = teamCreationRequest.getTeamName().trim();
+        String lowerTeamName = teamName.toLowerCase();
+        if (teamRepository.existsByTeamNameIgnoreCase(lowerTeamName))
+            throw new RuntimeException(); // TODO: Change In Exceptions Section
+
+        Team team = new Team(lowerTeamName);
+        team.setPresentationName(teamName);
+
+        teamRepository.save(team);
     }
 
+//    @PreAuthorize("hasRole('ADMIN')") TODO: In Future
     public void assignManagerToTeam() {
 
     }
 
+//    @PreAuthorize("hasRole('ADMIN')") TODO: In Future
     public void removeManagerFromTeam() {
 
     }
 
+//    @PreAuthorize("hasRole('ADMIN')") TODO: In Future
     public void archiveTeam() {
 
     }
 
+//    @PreAuthorize("hasRole('ADMIN')") TODO: In Future
     public void addToTeam() {
 
     }
 
+//    @PreAuthorize("hasRole('ADMIN')") TODO: In Future
     public void removeFromTeam() {
 
     }
