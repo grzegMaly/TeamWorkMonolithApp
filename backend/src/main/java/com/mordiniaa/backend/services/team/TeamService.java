ec8bab59499c79d3b71e3e0001a9274407ff9f69
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-09 00:24:42 +0100

    Service Method

--- backend/src/main/java/com/mordiniaa/backend/services/team/TeamService.java
+++ backend/src/main/java/com/mordiniaa/backend/services/team/TeamService.java
@@ -1,9 +1,22 @@
 package com.mordiniaa.backend.services.team;
 
+import com.mordiniaa.backend.models.team.Team;
+import com.mordiniaa.backend.repositories.mysql.TeamRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
+@RequiredArgsConstructor
 public class TeamService {
+
+    private final TeamRepository teamRepository;
+
+    Team getTeam(UUID teamId) {
+        return teamRepository.findById(teamId)
+                .orElseThrow(RuntimeException::new); // TODO: Change In Exceptions Section
+    }
 
     //Protected On Method Lvl
     public void getTeamsForManager() {
