8210ef406d4e5737aa1a960c0a02bb3128e05ae6
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-09 00:07:36 +0100

    Assign Manager Method Implemented

--- backend/src/main/java/com/mordiniaa/backend/services/team/TeamAdminService.java
+++ backend/src/main/java/com/mordiniaa/backend/services/team/TeamAdminService.java
@@ -1,17 +1,23 @@
 package com.mordiniaa.backend.services.team;
 
 import com.mordiniaa.backend.models.team.Team;
+import com.mordiniaa.backend.models.user.mysql.AppRole;
+import com.mordiniaa.backend.models.user.mysql.User;
 import com.mordiniaa.backend.repositories.mysql.TeamRepository;
+import com.mordiniaa.backend.repositories.mysql.UserRepository;
 import com.mordiniaa.backend.request.team.TeamCreationRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class TeamAdminService {
 
     private final TeamRepository teamRepository;
+    private final UserRepository userRepository;
 
     //Protected On Method Lvl For ADMIN
     @Transactional
@@ -29,27 +35,48 @@ public class TeamAdminService {
         teamRepository.save(team);
     }
 
-//    @PreAuthorize("hasRole('ADMIN')") TODO: In Future
-    public void assignManagerToTeam() {
+    @Transactional
+    //    @PreAuthorize("hasRole('ADMIN')") TODO: In Future
+    public void assignManagerToTeam(UUID userId, UUID teamId) {
 
+        User user = userRepository.findById(userId)
+                .orElseThrow(RuntimeException::new); // TODO: Change In Exceptions Section
+
+        if (!user.getRole().getAppRole().equals(AppRole.ROLE_MANAGER))
+            throw new RuntimeException(); // TODO: Change In Exceptions Section
+
+        Team team = teamRepository.findById(teamId)
+                .orElseThrow(RuntimeException::new); // TODO: Change In Exceptions Section
+
+        if (team.getManager() != null) {
+            User manager = team.getManager();
+            if (manager.getUserId().equals(userId))
+                // This manager Already Assigned
+                throw new RuntimeException(); // TODO: Change In Exceptions Section
+            // Other Manager Already Assigned
+            throw new RuntimeException(); // TODO: Change In Exceptions Section
+        }
+
+        team.setManager(user);
+        teamRepository.save(team);
     }
 
-//    @PreAuthorize("hasRole('ADMIN')") TODO: In Future
+    //    @PreAuthorize("hasRole('ADMIN')") TODO: In Future
     public void removeManagerFromTeam() {
 
     }
 
-//    @PreAuthorize("hasRole('ADMIN')") TODO: In Future
+    //    @PreAuthorize("hasRole('ADMIN')") TODO: In Future
     public void archiveTeam() {
 
     }
 
-//    @PreAuthorize("hasRole('ADMIN')") TODO: In Future
+    //    @PreAuthorize("hasRole('ADMIN')") TODO: In Future
     public void addToTeam() {
 
     }
 
-//    @PreAuthorize("hasRole('ADMIN')") TODO: In Future
+    //    @PreAuthorize("hasRole('ADMIN')") TODO: In Future
     public void removeFromTeam() {
 
     }
