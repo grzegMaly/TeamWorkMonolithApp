bee92fb84f3c0fbaa2f010ada64eee831cf587aa
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-08 23:58:34 +0100

    Creation Request

--- backend/src/main/java/com/mordiniaa/backend/request/team/TeamCreationRequest.java
+++ backend/src/main/java/com/mordiniaa/backend/request/team/TeamCreationRequest.java
@@ -1,4 +1,21 @@
 package com.mordiniaa.backend.request.team;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TeamCreationRequest {
+
+    @NotBlank
+    @Size(min = 5, max = 40)
+    @Pattern(regexp = "^\\p{L}+([ -]\\p{L}+)*$")
+    private String teamName;
 }
