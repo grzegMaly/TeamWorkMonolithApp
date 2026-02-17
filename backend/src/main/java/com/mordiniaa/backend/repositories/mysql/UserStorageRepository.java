f6d28ed3d2c7f05249ac6be1bce07131d2550950
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-17 23:47:52 +0100

    User Storage Repository

---
+++ backend/src/main/java/com/mordiniaa/backend/repositories/mysql/UserStorageRepository.java
@@ -0,0 +1,11 @@
+package com.mordiniaa.backend.repositories.mysql;
+
+import com.mordiniaa.backend.models.file.UserStorage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserStorageRepository extends JpaRepository<UserStorage, UUID> {
+}
