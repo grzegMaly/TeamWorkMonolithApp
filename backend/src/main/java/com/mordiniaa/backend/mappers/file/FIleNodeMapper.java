02e4267e01a566de1d5e39f90f471d158fb4e492
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-17 23:44:30 +0100

    Node Mapper

---
+++ backend/src/main/java/com/mordiniaa/backend/mappers/file/FIleNodeMapper.java
@@ -0,0 +1,20 @@
+package com.mordiniaa.backend.mappers.file;
+
+import com.mordiniaa.backend.dto.file.FileNodeDto;
+import com.mordiniaa.backend.models.file.FileNode;
+import com.mordiniaa.backend.models.file.NodeType;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FIleNodeMapper {
+
+    public FileNodeDto toDto(FileNode fileNode, String parentPath) {
+        FileNodeDto dto = new FileNodeDto();
+        dto.setNodeType(fileNode.getNodeType());
+        dto.setId(fileNode.getId());
+        dto.setParentPath(parentPath);
+        dto.setSize(fileNode.getNodeType().equals(NodeType.DIRECTORY) ? fileNode.getSubTreeSize() : fileNode.getSize());
+        dto.setName(fileNode.getName());
+        return dto;
+    }
+}
