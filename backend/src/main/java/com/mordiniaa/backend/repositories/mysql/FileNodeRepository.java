7bfa8e7ce0dd84d384d2d3973a0d5f3aae25b778
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-17 23:48:39 +0100

    FindDirByIdAndOwnerId

---
+++ backend/src/main/java/com/mordiniaa/backend/repositories/mysql/FileNodeRepository.java
@@ -0,0 +1,23 @@
+package com.mordiniaa.backend.repositories.mysql;
+
+import com.mordiniaa.backend.models.file.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+@Repository
+public interface FileNodeRepository extends JpaRepository<FileNode, UUID> {
+
+    @Query("""
+            select fn
+            from FileNode fn
+            where fn.deleted = false and fn.id = :id and fn.userStorage.userId = :userId and fn.nodeType = 'DIRECTORY' or fn.nodeType = 'ROOT'
+            """)
+    Optional<FileNode> findDirByIdAndOwnerId(UUID id, UUID userId);
+}
