5319a4fffc114ed8b9a1e5848fbac311d2691133
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-19 00:06:11 +0100

    Repo Method Added

--- backend/src/main/java/com/mordiniaa/backend/repositories/mongo/ImageMetadataRepository.java
+++ backend/src/main/java/com/mordiniaa/backend/repositories/mongo/ImageMetadataRepository.java
@@ -4,5 +4,9 @@ import com.mordiniaa.backend.models.file.imageStorage.ImageMetadata;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface ImageMetadataRepository extends MongoRepository<ImageMetadata, ObjectId> {
+    Optional<ImageMetadata> findImageMetadataByOwnerId(UUID ownerId);
 }
