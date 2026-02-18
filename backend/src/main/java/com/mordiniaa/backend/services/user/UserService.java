18671be13fa1b8b029e6bdee131435cb7e73ad27
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-19 00:10:25 +0100

    Add Profile Image Method Implemented

--- backend/src/main/java/com/mordiniaa/backend/services/user/UserService.java
+++ backend/src/main/java/com/mordiniaa/backend/services/user/UserService.java
@@ -1,12 +1,24 @@
 package com.mordiniaa.backend.services.user;
 
+import com.mordiniaa.backend.config.StorageProperties;
+import com.mordiniaa.backend.models.file.imageStorage.ImageMetadata;
+import com.mordiniaa.backend.models.user.DbUser;
 import com.mordiniaa.backend.models.user.mysql.User;
+import com.mordiniaa.backend.repositories.mongo.ImageMetadataRepository;
 import com.mordiniaa.backend.repositories.mongo.user.UserRepresentationRepository;
 import com.mordiniaa.backend.repositories.mysql.UserRepository;
+import com.mordiniaa.backend.services.storage.StorageProvider;
+import com.mordiniaa.backend.services.storage.profileImagesStorage.ImagesStorageService;
+import com.mordiniaa.backend.utils.CloudStorageServiceUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.InputStream;
 import java.util.UUID;
 
 @Service
@@ -14,7 +26,14 @@ import java.util.UUID;
 public class UserService {
 
     private final UserRepository userRepository;
-    private UserRepresentationRepository userRepresentationRepository;
+    private final UserRepresentationRepository userRepresentationRepository;
+    private final MongoUserService mongoUserService;
+    private final StorageProperties storageProperties;
+    private final CloudStorageServiceUtils cloudStorageServiceUtils;
+    private final StorageProvider storageProvider;
+    private final ImageMetadataRepository imageMetadataRepository;
+    private final MongoTemplate mongoTemplate;
+    private final ImagesStorageService imagesStorageService;
 
     public User getUser(UUID userId) {
         return userRepository.findById(userId)
@@ -23,6 +42,11 @@ public class UserService {
 
     public void addProfileImage(UUID userId, MultipartFile file) {
 
+        mongoUserService.checkUserAvailability(userId);
+        DbUser user = userRepresentationRepository.findByUserId(userId)
+                .orElseThrow(RuntimeException::new); // TODO: Change In Exceptions Section
+
+        imagesStorageService.addProfileImage(user, file);
     }
 
     public void setDefaultProfileImage() {
