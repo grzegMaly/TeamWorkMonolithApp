7919612b69dd5502b4feb9683b403d757eec1d13
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-19 00:06:52 +0100

    Modifying Repo Method Added

--- backend/src/main/java/com/mordiniaa/backend/repositories/mysql/UserRepository.java
+++ backend/src/main/java/com/mordiniaa/backend/repositories/mysql/UserRepository.java
@@ -2,10 +2,15 @@ package com.mordiniaa.backend.repositories.mysql;
 
 import com.mordiniaa.backend.models.user.mysql.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    @Modifying
+    @Query("update User u set u.imageKey = :imageKey where u.userId = :userId")
+    void updateImageKeyByUserId(String imageKey, UUID userId);
 }
