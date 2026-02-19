3d7c9072599c4d79602107f3bce262670fcce610
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-20 00:20:23 +0100

    Test Fix

--- backend/src/test/java/com/mordiniaa/backend/services/task/TaskManagementUpdateTaskRepoTest.java
+++ backend/src/test/java/com/mordiniaa/backend/services/task/TaskManagementUpdateTaskRepoTest.java
@@ -1,10 +1,6 @@
 package com.mordiniaa.backend.services.task;
 
 import com.mordiniaa.backend.dto.task.TaskDetailsDTO;
-import com.mordiniaa.backend.mappers.task.TaskMapper;
-import com.mordiniaa.backend.mappers.task.activityMappers.TaskActivityMapper;
-import com.mordiniaa.backend.mappers.task.activityMappers.dtoMappers.TaskCommentDtoMapper;
-import com.mordiniaa.backend.mappers.user.UserRepresentationMapper;
 import com.mordiniaa.backend.models.board.Board;
 import com.mordiniaa.backend.models.board.BoardMember;
 import com.mordiniaa.backend.models.board.TaskCategory;
@@ -88,17 +84,17 @@ public class TaskManagementUpdateTaskRepoTest {
         owner = new UserRepresentation();
         owner.setUserId(ownerId);
         owner.setUsername("Owner");
-        owner.setImageUrl("http:random123.com");
+        owner.setImageKey("http:random123.com");
 
         user1 = new UserRepresentation();
         user1.setUserId(member1Id);
         user1.setUsername("Member 1");
-        user1.setImageUrl("http:random123.com");
+        user1.setImageKey("http:random123.com");
 
         user2 = new UserRepresentation();
         user2.setUserId(member2Id);
         user2.setUsername("Member 2");
-        user2.setImageUrl("http:random123.com");
+        user2.setImageKey("http:random123.com");
 
         taskCategory1 = new TaskCategory();
         taskCategory1.setCategoryName("Dev");
@@ -273,7 +269,7 @@ public class TaskManagementUpdateTaskRepoTest {
         UserRepresentation user = new UserRepresentation();
         user.setUserId(userId);
         user.setUsername("Username");
-        user.setImageUrl("https://random123.com");
+        user.setImageKey("https://random123.com");
         userRepresentationRepository.save(user);
 
         BoardMember member = new BoardMember(userId);
