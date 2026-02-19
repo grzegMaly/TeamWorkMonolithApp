9a50bfed0e054afb434283b515c4044f066b24f2
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-19 17:02:08 +0100

    Role Repo Extended

--- backend/src/main/java/com/mordiniaa/backend/repositories/mysql/RoleRepository.java
+++ backend/src/main/java/com/mordiniaa/backend/repositories/mysql/RoleRepository.java
@@ -1,9 +1,13 @@
 package com.mordiniaa.backend.repositories.mysql;
 
+import com.mordiniaa.backend.models.user.mysql.AppRole;
 import com.mordiniaa.backend.models.user.mysql.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findRoleByAppRole(AppRole appRole);
 }
