1ad07d7ec4e738c5ee2ab4f015c1e9010e3622e9
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-19 17:01:51 +0100

    Contact Entity Implemented

--- backend/src/main/java/com/mordiniaa/backend/models/user/mysql/Contact.java
+++ backend/src/main/java/com/mordiniaa/backend/models/user/mysql/Contact.java
@@ -1,4 +1,23 @@
 package com.mordiniaa.backend.models.user.mysql;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String countryCallingCode;
+    private String phoneNumber;
+    private String email;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
 }
