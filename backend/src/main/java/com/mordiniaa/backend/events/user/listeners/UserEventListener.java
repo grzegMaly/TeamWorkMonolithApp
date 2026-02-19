cd44c8ce50b05cc5b6a4b33f5bc6b9851af8b07f
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-19 17:23:30 +0100

    Event Handler For Chaning Image

--- backend/src/main/java/com/mordiniaa/backend/events/user/listeners/UserEventListener.java
+++ backend/src/main/java/com/mordiniaa/backend/events/user/listeners/UserEventListener.java
@@ -1,12 +1,17 @@
 package com.mordiniaa.backend.events.user.listeners;
 
 import com.mordiniaa.backend.events.user.events.UserCreatedEvent;
+import com.mordiniaa.backend.events.user.events.UserProfileImageChangedEvent;
 import com.mordiniaa.backend.models.user.mongodb.UserRepresentation;
 import com.mordiniaa.backend.models.user.mysql.User;
 import com.mordiniaa.backend.repositories.mongo.user.UserRepresentationRepository;
 import com.mordiniaa.backend.repositories.mysql.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +25,7 @@ public class UserEventListener {
 
     private final UserRepository userRepository;
     private final UserRepresentationRepository userRepresentationRepository;
+    private final MongoTemplate mongoTemplate;
 
     @Async
     @Transactional(readOnly = true)
@@ -36,5 +42,19 @@ public class UserEventListener {
         userRepresentationRepository.save(mongoUser);
 
         log.info("Mongo projection created for user: {}", event.userId());
+    }
+
+    @Async
+    @Transactional
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handle(UserProfileImageChangedEvent event) {
+
+        Query query = Query.query(
+                Criteria.where("userId").is(event.userId())
+        );
+        Update update = new Update()
+                .set("imageKey", event.imageKey());
+
+        mongoTemplate.updateFirst(query, update, UserRepresentation.class);
     }
 }
