d0594ebe5d733f13c59bdabad3ee8166386af4c3
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-19 19:39:03 +0100

    Repo Method Changed

--- backend/src/main/java/com/mordiniaa/backend/repositories/mysql/AddressRepository.java
+++ backend/src/main/java/com/mordiniaa/backend/repositories/mysql/AddressRepository.java
@@ -1,15 +1,15 @@
 package com.mordiniaa.backend.repositories.mysql;
 
 import com.mordiniaa.backend.models.user.mysql.Address;
+import com.mordiniaa.backend.models.user.mysql.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-import java.util.UUID;
 
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
 
-    Optional<Address> findByIdAndUser_UserId(Long id, UUID userUserId);
+    Optional<Address> findByIdAndUser(Long id, User user);
 }
