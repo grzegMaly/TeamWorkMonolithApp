636ab0c34dfd0a06a56f0873143f551c460a860a
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-20 18:44:52 +0100

    Contact Annotations Added

--- backend/src/main/java/com/mordiniaa/backend/models/user/mysql/Contact.java
+++ backend/src/main/java/com/mordiniaa/backend/models/user/mysql/Contact.java
@@ -6,15 +6,21 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@Entity
+@Table(name = "contacts")
+@Entity(name = "Contact")
 public class Contact {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "country_calling_code", nullable = false, length = 3)
     private String countryCallingCode;
+
+    @Column(name = "phone_number", nullable = false, length = 14)
     private String phoneNumber;
+
+    @Column(name = "email", nullable = false, length = 50)
     private String email;
 
     @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
