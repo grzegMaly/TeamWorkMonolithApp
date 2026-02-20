ed4bc5b6520abdeb2eb16538640362e89b00140a
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-20 12:26:17 +0100

    Prod Files

---
+++ backend/mongo/init/prod/01-users.js
@@ -0,0 +1,11 @@
+use team-work
+db.createUser({
+    user: "team-work-admin",
+    roles: [
+        {role: "dbAdmin", db: "team-work"},
+        {role: "dbOwner", db: "team-work"},
+        {role: "userAdmin", db: "team-work"}
+    ],
+    pwd: "admin"
+})
+db.auth("team-work-admin", "admin")
