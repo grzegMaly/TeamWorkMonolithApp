512e62ce73baf1c464a7693ffaf39c403bf2b6b5
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-20 12:26:22 +0100

    Test Files

---
+++ backend/mongo/init/test/01-users.js
@@ -0,0 +1,9 @@
+use team-work-test
+db.createUser({
+    user: "team-work-tester",
+    roles: [
+        {role: "dbOwner", db: "team-work-test"},
+        {role: "dbAdmin", db: "team-work-test"}
+    ],
+    pwd: "password"
+})
---
+++ backend/mongo/init/test/03-indexes.js
@@ -0,0 +1,30 @@
+use team-work-tester
+db.auth("team-work-test", "password")
+
+db.notes.createIndex(
+    {
+        "title": "text",
+        "content": "text"
+    }
+)
+
+db.boards.createIndex({
+  "members.userId": 1,
+  teamId: 1
+})
+
+db.boards.createIndex({
+  "owner.userId": 1,
+  deleted: 1,
+  archived: 1
+})
+
+db.tasks.createIndex(
+    {
+        title: 1
+    }
+)
+
+db.users.createIndex({
+    userId: 1
+})
