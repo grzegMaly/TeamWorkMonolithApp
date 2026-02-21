cc84bad517491774d57c6d7fe5db1614472a65a9
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-21 23:29:13 +0100

    Board Admin Controller Template

---
+++ backend/src/main/java/com/mordiniaa/backend/controllers/admin/BoardAdminController.java
@@ -0,0 +1,21 @@
+package com.mordiniaa.backend.controllers.admin;
+
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/admin/board")
+public class BoardAdminController {
+
+    @PutMapping
+    public void setBoardOwner(@RequestParam(name = "u") UUID userId,
+                              @RequestParam(name = "t") UUID teamId,
+                              @RequestParam(name = "b") String boardId
+    ) {
+
+    }
+}
