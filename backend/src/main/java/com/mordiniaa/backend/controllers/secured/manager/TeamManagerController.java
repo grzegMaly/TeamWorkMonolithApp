fdac888d30596b180e116dee465aa16422aa9fc7
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-22 00:34:07 +0100

    Team Manager Controller Template

---
+++ backend/src/main/java/com/mordiniaa/backend/controllers/secured/manager/TeamManagerController.java
@@ -0,0 +1,17 @@
+package com.mordiniaa.backend.controllers.secured.manager;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/manager/team")
+public class TeamManagerController {
+
+    public void getTeams() {
+
+    }
+
+    public void getTeamDetails() {
+
+    }
+}
