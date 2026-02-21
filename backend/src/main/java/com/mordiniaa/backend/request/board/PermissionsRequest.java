4bd3bb03cbed31d792d550791ce556bb7ce97183
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-22 00:34:40 +0100

    Request Update

--- backend/src/main/java/com/mordiniaa/backend/request/board/PermissionsRequest.java
+++ backend/src/main/java/com/mordiniaa/backend/request/board/PermissionsRequest.java
@@ -4,6 +4,7 @@ import com.mordiniaa.backend.models.board.permissions.BoardPermission;
 import com.mordiniaa.backend.models.board.permissions.CategoryPermissions;
 import com.mordiniaa.backend.models.board.permissions.CommentPermission;
 import com.mordiniaa.backend.models.board.permissions.TaskPermission;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,8 +16,15 @@ import java.util.Set;
 @NoArgsConstructor
 public class PermissionsRequest {
 
+    @NotNull
     private Set<BoardPermission> boardPermissions;
+
+    @NotNull
     private Set<CategoryPermissions> categoryPermissions;
+
+    @NotNull
     private Set<TaskPermission> taskPermissions;
+
+    @NotNull
     private Set<CommentPermission> commentPermissions;
 }
