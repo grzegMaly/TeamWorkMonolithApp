0cf9dd72255c370b0237f7affa3957e157b0d3c5
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-22 00:34:20 +0100

    Request Update

--- backend/src/main/java/com/mordiniaa/backend/request/board/BoardCreationRequest.java
+++ backend/src/main/java/com/mordiniaa/backend/request/board/BoardCreationRequest.java
@@ -1,5 +1,8 @@
 package com.mordiniaa.backend.request.board;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +14,10 @@ import java.util.UUID;
 @NoArgsConstructor
 public class BoardCreationRequest {
 
+    @NotNull
     private UUID teamId;
+
+    @NotBlank
+    @Size(min = 5, max = 40)
     private String boardName;
 }
