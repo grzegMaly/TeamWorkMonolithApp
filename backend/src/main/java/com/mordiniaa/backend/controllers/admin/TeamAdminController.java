69da77af4ec9889dba1d0af3486df263657ce604
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-21 23:29:20 +0100

    Team Admin Controller Template

---
+++ backend/src/main/java/com/mordiniaa/backend/controllers/admin/TeamAdminController.java
@@ -0,0 +1,37 @@
+package com.mordiniaa.backend.controllers.admin;
+
+import com.mordiniaa.backend.request.team.TeamCreationRequest;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/admin/team")
+public class TeamAdminController {
+
+    public void createTeam(@Valid TeamCreationRequest teamCreationRequest) {
+
+    }
+
+    public void assignManagerToTeam(UUID userId, UUID teamId) {
+
+    }
+
+    public void removeManagerFromTeam(UUID teamId) {
+
+    }
+
+    public void archiveTeam(UUID teamId) {
+
+    }
+
+    public void addToTeam(UUID userId, UUID teamId) {
+
+    }
+
+    public void removeFromTeam(UUID userId, UUID teamId) {
+
+    }
+}
