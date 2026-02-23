82b618b70cc9e64294d7df558d37f9cac1a3c2d4
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-23 14:00:08 +0100

    Security User Query

--- backend/src/main/java/com/mordiniaa/backend/repositories/mysql/UserRepository.java
+++ backend/src/main/java/com/mordiniaa/backend/repositories/mysql/UserRepository.java
@@ -1,11 +1,14 @@
 package com.mordiniaa.backend.repositories.mysql;
 
+import com.mordiniaa.backend.models.user.mysql.AppRole;
 import com.mordiniaa.backend.models.user.mysql.User;
+import com.mordiniaa.backend.security.service.user.SecurityUserProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -21,4 +24,23 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     @Modifying
     @Query("update User u set u.deleted = :deleted where u.userId = :userId")
     void updateDeletedByUserId(boolean deleted, UUID userId);
+
+    @Query("select count(*) from User u where u.role.appRole = :appRole")
+    int countByRole_AppRole(AppRole appRole);
+
+    Optional<User> findUsersByRole_AppRole(AppRole roleAppRole);
+
+    @Query("""
+            select u.userId as userId,
+            u.username as username,
+            u.password as password,
+            u.role as role,
+            u.accountNonExpired as accountNonExpired,
+            u.accountNonLocked as accountNonLocked,
+            u.credentialsNonExpired as credentialsNonExpired,
+            u.deleted as deleted
+            from User u
+            where u.username = :username
+            """)
+    Optional<SecurityUserProjection> findSecurityUserByUsername(String username);
 }
