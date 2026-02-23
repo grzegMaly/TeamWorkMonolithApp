5d0a1f0de3244d02db5069ea10335ed9d1628c5c
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-23 23:57:45 +0100

    Temp Security Config

---
+++ backend/src/main/java/com/mordiniaa/backend/security/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.mordiniaa.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true
+)
+public class SecurityConfig {
+
+    @Bean
+    BCryptPasswordEncoder bcryptEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf ->
+                        csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                                .ignoringRequestMatchers("/sockjs/**")
+                                .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler())
+                )
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(requests ->
+                        requests
+                                .requestMatchers("/api/csrf-token").permitAll()
+                                .requestMatchers("/api/v1/test/**").permitAll()
+                )
+                .build();
+    }
+}
