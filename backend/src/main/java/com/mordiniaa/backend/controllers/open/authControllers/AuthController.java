6819e0b2372ff59041403995930076b35629613b
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-24 22:04:53 +0100

    Login Controller Method

---
+++ backend/src/main/java/com/mordiniaa/backend/controllers/open/authControllers/AuthController.java
@@ -0,0 +1,70 @@
+package com.mordiniaa.backend.controllers.open.authControllers;
+
+import com.mordiniaa.backend.exceptions.BadCredentialsException;
+import com.mordiniaa.backend.request.auth.LoginRequest;
+import com.mordiniaa.backend.response.user.UserInfoResponse;
+import com.mordiniaa.backend.services.auth.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    private final AuthService authService;
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping("/signin")
+    public ResponseEntity<Void> login(LoginRequest loginRequest) {
+
+        Authentication authentication;
+        try {
+            authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            loginRequest.getUsername(),
+                            loginRequest.getPassword()
+                    )
+            );
+        } catch (Exception e) {
+            throw new BadCredentialsException();
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        HttpHeaders headers = authService.authenticate(authentication)
+                .stream()
+                .collect(
+                        HttpHeaders::new,
+                        (h, c) -> h.add(HttpHeaders.SET_COOKIE, c.toString()),
+                        HttpHeaders::addAll
+                );
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .build();
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<UserInfoResponse> getUserDetails() {
+        UserInfoResponse infoResponse = authService.getUserDetails(UUID.randomUUID());
+        return ResponseEntity.ok(infoResponse);
+    }
+
+    @PostMapping("/signout")
+    public ResponseEntity<?> signOut() {
+
+        return null;
+    }
+}
