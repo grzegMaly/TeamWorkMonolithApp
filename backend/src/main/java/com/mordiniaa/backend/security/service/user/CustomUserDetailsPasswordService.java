3542b0fc4cb2359c014db8204491c43b5beda2c4
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-25 00:36:10 +0100

    Custom User Details Password Service

---
+++ backend/src/main/java/com/mordiniaa/backend/security/service/user/CustomUserDetailsPasswordService.java
@@ -0,0 +1,30 @@
+package com.mordiniaa.backend.security.service.user;
+
+import com.mordiniaa.backend.repositories.mysql.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsPasswordService;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsPasswordService implements UserDetailsPasswordService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails updatePassword(UserDetails user, String newPassword) {
+
+        SecurityUser securityUser = (SecurityUser) user;
+        UUID userId = securityUser.getUserId();
+
+        userRepository.updatePasswordByUserId(userId, newPassword);
+
+        SecurityUserProjection updatedUser = userRepository.findSecurityUserByUsername(securityUser.getUsername())
+                .orElseThrow(RuntimeException::new);
+
+        return SecurityUser.build(updatedUser);
+    }
+}
