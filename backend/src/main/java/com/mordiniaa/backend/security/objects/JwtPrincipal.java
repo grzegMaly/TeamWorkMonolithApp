a86538aaba8163e2c8bffd0c192e82a097bd9743
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-25 17:38:37 +0100

    Jwt Principal

---
+++ backend/src/main/java/com/mordiniaa/backend/security/objects/JwtPrincipal.java
@@ -0,0 +1,12 @@
+package com.mordiniaa.backend.security.objects;
+
+import java.util.List;
+import java.util.UUID;
+
+public record JwtPrincipal(
+        UUID userId,
+        UUID sessionId,
+        List<String> roles,
+        String refreshToken
+) {
+}
