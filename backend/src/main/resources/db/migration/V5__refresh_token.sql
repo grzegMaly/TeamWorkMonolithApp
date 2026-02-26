0c8d3b56a3558b053d4291feb29a358f73759f65
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-26 14:47:33 +0100

    Refresh Token Family Added To Migration

--- backend/src/main/resources/db/migration/V5__refresh_token.sql
+++ backend/src/main/resources/db/migration/V5__refresh_token.sql
@@ -1,23 +1,38 @@
 use `team-work`;
 
+create table `refresh_token_families`
+(
+    `id`         bigint auto_increment not null,
+    `version`    bigint                not null,
+    `user_id`    binary(16)            null,
+    `created_at` timestamp             not null,
+    `expires_at` timestamp             not null,
+    `revoked`    boolean               not null default false,
+    `revoked_at` timestamp             null,
+    constraint `pk_families` primary key (`id`)
+);
+
+create index `idx_rtf_user` on `refresh_token_families` (`user_id`);
+
 create table `refresh_tokens`
 (
-    `id`                bigint auto_increment not null,
-    `version`           bigint                not null,
-    `user_id`           binary(16)            not null,
-    `hashed_token`      varchar(255)          not null,
-    `revoked`           boolean               not null,
-    `created_at`        timestamp             not null,
-    `expires_at`        timestamp             not null,
-    `revoked_at`        timestamp             null,
-    `parent_id`         bigint                null,
-    `family_id`         binary(16)            not null,
-    `family_expires_at` timestamp             not null,
-    `replaced_by_id`    bigint,
-    `roles`             JSON                  not null,
+    `id`                   bigint auto_increment not null,
+    `version`              bigint                not null,
+    `refresh_token_family` bigint                not null,
+    `hashed_token`         varchar(255)          not null,
+    `revoked`              boolean               not null default false,
+    `created_at`           timestamp             not null,
+    `expires_at`           timestamp             not null,
+    `revoked_at`           timestamp             null,
+    `parent_id`            bigint                null,
+    `replaced_by_id`       bigint,
+    `roles`                JSON                  not null,
     constraint `pk_rt` primary key (`id`)
 );
 
-create index `idx_rt_user` on `refresh_tokens` (`user_id`);
-create index `idx_rt_family` on `refresh_tokens` (`user_id`);
-create index `idx_rt_revoked` on `refresh_tokens` (`revoked`);
+alter table `refresh_tokens`
+    add constraint `fk_rt_rtf_id` foreign key (`refresh_token_family`)
+        references `refresh_token_families` (`id`) on delete cascade;
+
+create index `idx_rt_family_revoked`
+    on `refresh_tokens` (`refresh_token_family`, `revoked`);
