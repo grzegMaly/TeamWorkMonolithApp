8e5885ac79c0f06be3c55327baa3c7ce229d0c77
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-27 00:43:04 +0100

    Rotate Refresh Token Valid Test

--- backend/src/test/java/com/mordiniaa/backend/security/service/token/RefreshTokenServiceTest.java
+++ backend/src/test/java/com/mordiniaa/backend/security/service/token/RefreshTokenServiceTest.java
@@ -1,17 +1,22 @@
 package com.mordiniaa.backend.security.service.token;
 
 import com.mordiniaa.backend.repositories.mysql.RefreshTokenFamilyRepository;
+import com.mordiniaa.backend.repositories.mysql.RefreshTokenRepository;
 import com.mordiniaa.backend.security.model.RefreshTokenEntity;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.Instant;
 import java.util.List;
 import java.util.Random;
 import java.util.UUID;
+import java.util.concurrent.Executor;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,13 +32,26 @@ public class RefreshTokenServiceTest {
     @Autowired
     private RawTokenService rawTokenService;
 
+    @Autowired
+    private RefreshTokenRepository refreshTokenRepository;
+
     @AfterEach
     void tearDown() {
         refreshTokenFamilyRepository.deleteAll();
     }
 
+    @TestConfiguration
+    @EnableAsync(proxyTargetClass = true)
+    static class SyncAsyncTestConfig {
+
+        @Bean
+        public Executor taskExecutor() {
+            return Runnable::run;
+        }
+    }
+
     @Test
-    void generateRefreshTokenEntity() {
+    void generateRefreshTokenEntityTest() {
 
         UUID userId = UUID.randomUUID();
         Long familyId = new Random().nextLong();
@@ -50,5 +68,37 @@ public class RefreshTokenServiceTest {
         assertNotNull(entity);
         assertFalse(entity.isRevoked());
         assertTrue(entity.getExpiresAt().toEpochMilli() > Instant.now().toEpochMilli());
+    }
+
+    @Test
+    void rotateTokenValidTest() {
+
+        UUID userId = UUID.randomUUID();
+        Long familyId = new Random().nextLong();
+        String rawToken = rawTokenService.generateOpaqueToken();
+        List<String> roles = List.of("ROLE_ADMIN");
+
+        RefreshTokenEntity entity = refreshTokenService.generateRefreshTokenEntity(
+                userId,
+                familyId,
+                rawToken,
+                roles
+        );
+
+        Long savedFamilyId = entity.getRefreshTokenFamily().getId();
+
+        Long tokenId = entity.getId();
+        String newToken = rawTokenService.generateOpaqueToken();
+
+        RefreshTokenEntity rotatedEntity = refreshTokenService.rotate(userId, tokenId, rawToken, newToken, roles);
+        assertNotNull(rotatedEntity);
+        assertEquals(roles, rotatedEntity.getRoles());
+
+        RefreshTokenEntity revokedEntity = refreshTokenRepository.findById(tokenId)
+                .orElseThrow();
+        assertTrue(revokedEntity.isRevoked());
+        assertTrue(revokedEntity.getRevokedAt().isBefore(Instant.now()));
+
+        assertEquals(savedFamilyId, rotatedEntity.getRefreshTokenFamily().getId());
     }
 }
