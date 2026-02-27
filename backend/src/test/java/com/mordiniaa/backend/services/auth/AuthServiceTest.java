d590dff7731ca86ba3b4cab874b80c787c722150
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-02-27 19:34:17 +0100

    Refresh Token Invalid Refresh Token Test

--- backend/src/test/java/com/mordiniaa/backend/services/auth/AuthServiceTest.java
+++ backend/src/test/java/com/mordiniaa/backend/services/auth/AuthServiceTest.java
@@ -1,8 +1,8 @@
 package com.mordiniaa.backend.services.auth;
 
 import com.mordiniaa.backend.repositories.mysql.RefreshTokenFamilyRepository;
-import com.mordiniaa.backend.repositories.mysql.UserRepository;
 import com.mordiniaa.backend.security.objects.JwtPrincipal;
+import com.mordiniaa.backend.security.service.token.RawTokenService;
 import com.mordiniaa.backend.security.utils.JwtUtils;
 import io.jsonwebtoken.Jwts;
 import org.junit.jupiter.api.AfterEach;
@@ -14,17 +14,16 @@ import org.springframework.data.redis.core.Cursor;
 import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.ResponseCookie;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -46,6 +45,8 @@ public class AuthServiceTest {
     private StringRedisTemplate stringRedisTemplate;
     @Autowired
     private JwtUtils jwtUtils;
+    @Autowired
+    private RawTokenService rawTokenService;
 
     @AfterEach
     void tearDown() {
@@ -209,6 +210,102 @@ public class AuthServiceTest {
 
         Authentication jwtAuthentication = new UsernamePasswordAuthenticationToken(
                 new JwtPrincipal(userId, sessionId, roles, refreshToken),
+                null,
+                List.of()
+        );
+
+        assertThrows(RuntimeException.class,
+                () -> authService.refresh(jwtAuthentication));
+    }
+
+    @Test
+    @DisplayName("Refresh Token Invalid Refresh Token Id Test")
+    void refreshTokenInvalidRefreshTokenIdTest() {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        "admin",
+                        "superSecretPassword"
+                )
+        );
+
+        List<ResponseCookie> cookies = authService.authenticate(authentication);
+
+        ResponseCookie jwtCookie = cookies.stream().filter(cookie -> cookie.getName().equals("TEAMWORK-ACCESS"))
+                .findFirst().orElseThrow();
+
+        ResponseCookie refreshCookie = cookies.stream().filter(cookie -> cookie.getName().equals("TEAMWORK-REFRESH"))
+                .findFirst().orElseThrow();
+
+        String jwtToken = jwtCookie.getValue();
+        String refreshToken = refreshCookie.getValue();
+
+        String newRefreshToken = new Random().nextLong() + refreshToken.substring(refreshToken.indexOf("."));
+        UUID userId = UUID.fromString(Jwts.parser()
+                .verifyWith((SecretKey) jwtUtils.key())
+                .build().parseSignedClaims(jwtToken)
+                .getPayload().getSubject());
+
+        UUID sessionId = UUID.fromString((String) Jwts.parser()
+                .verifyWith((SecretKey) jwtUtils.key())
+                .build().parseSignedClaims(jwtToken)
+                .getPayload().get("sid"));
+
+        List<String> roles = List.of((String) Jwts.parser()
+                .verifyWith((SecretKey) jwtUtils.key())
+                .build().parseSignedClaims(jwtToken)
+                .getPayload().get("sid"));
+
+        Authentication jwtAuthentication = new UsernamePasswordAuthenticationToken(
+                new JwtPrincipal(userId, sessionId, roles, newRefreshToken),
+                null,
+                List.of()
+        );
+
+        assertThrows(RuntimeException.class,
+                () -> authService.refresh(jwtAuthentication));
+    }
+
+    @Test
+    @DisplayName("Refresh Token Invalid Refresh Token Test")
+    void refreshTokenInvalidRefreshTokenTest() {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        "admin",
+                        "superSecretPassword"
+                )
+        );
+
+        List<ResponseCookie> cookies = authService.authenticate(authentication);
+
+        ResponseCookie jwtCookie = cookies.stream().filter(cookie -> cookie.getName().equals("TEAMWORK-ACCESS"))
+                .findFirst().orElseThrow();
+
+        ResponseCookie refreshCookie = cookies.stream().filter(cookie -> cookie.getName().equals("TEAMWORK-REFRESH"))
+                .findFirst().orElseThrow();
+
+        String jwtToken = jwtCookie.getValue();
+        String refreshToken = refreshCookie.getValue();
+
+        String newRefreshToken = refreshToken.substring(0, refreshToken.indexOf(".") + 1) + rawTokenService.generateOpaqueToken();
+        UUID userId = UUID.fromString(Jwts.parser()
+                .verifyWith((SecretKey) jwtUtils.key())
+                .build().parseSignedClaims(jwtToken)
+                .getPayload().getSubject());
+
+        UUID sessionId = UUID.fromString((String) Jwts.parser()
+                .verifyWith((SecretKey) jwtUtils.key())
+                .build().parseSignedClaims(jwtToken)
+                .getPayload().get("sid"));
+
+        List<String> roles = List.of((String) Jwts.parser()
+                .verifyWith((SecretKey) jwtUtils.key())
+                .build().parseSignedClaims(jwtToken)
+                .getPayload().get("sid"));
+
+        Authentication jwtAuthentication = new UsernamePasswordAuthenticationToken(
+                new JwtPrincipal(userId, sessionId, roles, newRefreshToken),
                 null,
                 List.of()
         );
